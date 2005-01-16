153a826d88cf7551fc0ec59480bc350864b17aa1
Greg Chicares <gchicares@massmutual.com>
2005-01-16 08:00:07 +0000

    Add copyright, license, and RCS Id even for trivial files
    git-svn-id: svn://svn.sv.gnu.org/lmi/lmi/trunk@150 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- version.hpp
+++ version.hpp
@@ -1,1 +1,22 @@
-#define LMI_VERSION "20050113T2146Z"
+// Copyright (C) 2005 Gregory W. Chicares.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+// http://savannah.nongnu.org/projects/lmi
+// email: <chicares@cox.net>
+// snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
+
+// $Id: version.hpp,v 1.2 2005-01-16 08:00:07 chicares Exp $
+
+#define LMI_VERSION "20050116T0734Z"
