623e0540291f82007e2fdfe538ce894856f3dd0a
Greg Chicares <gchicares@massmutual.com>
2005-04-29 10:09:59 +0000

    Add path-utility functions and unit tests
    git-svn-id: svn://svn.sv.gnu.org/lmi/lmi/trunk@399 8995fb05-a277-41b4-a4d2-e84fd280a9a6

---
+++ path_utility.cpp
@@ -0,0 +1,94 @@
+// Path utilities.
+//
+// Copyright (C) 2004, 2005 Gregory W. Chicares.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+// http://savannah.nongnu.org/projects/lmi
+// email: <chicares@cox.net>
+// snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
+
+// $Id: path_utility.cpp,v 1.1 2005-04-29 10:09:59 chicares Exp $
+
+#ifdef __BORLANDC__
+#   include "pchfile.hpp"
+#   pragma hdrstop
+#endif // __BORLANDC__
+
+#include "path_utility.hpp"
+
+#include "alert.hpp"
+#include "miscellany.hpp" // iso_8601_datestamp_terse()
+
+#include <boost/filesystem/convenience.hpp>
+#include <boost/filesystem/exception.hpp>
+
+/// Create a unique file path, following input as closely as possible.
+///
+/// Compose a file path from 'directory' and 'filename', changing its
+/// extension, if any, to 'extension'. If the file path already exists,
+/// then try to remove it. If that fails, then try to make the file
+/// path unique by inserting a "YYYYMMDDTHHMMSSZ" timestamp before the
+/// extension. If even that does not produce a unique name, then throw
+/// an exception.
+///
+/// A try-block is necessary because fs::remove() can throw. It is not
+/// clear from the boost documentation happens if the file doesn't
+/// actually get removed, e.g. because it's write-protected, so this
+/// function checks the postcondition (file shouldn't exist) and, if
+/// it's not satisfied, explicitly throws an exception of the same
+/// type that the boost library function would throw if it enforces
+/// the postcondition, because both these exceptional conditions are
+/// handled the same way.
+
+fs::path unique_filepath
+    (fs::path const&    original_filepath
+    ,std::string const& extension
+    )
+{
+    fs::path filepath(original_filepath);
+    filepath = fs::change_extension(filepath, extension);
+    if(!fs::exists(filepath))
+        {
+        return filepath;
+        }
+
+    try
+        {
+        fs::remove(filepath);
+        if(fs::exists(filepath))
+            {
+            throw fs::filesystem_error("", "Removal failed");
+            }
+        }
+    catch(fs::filesystem_error const& e)
+        {
+        std::string basename = fs::basename(filepath);
+        basename += '-' + iso_8601_datestamp_terse() + extension;
+        filepath = filepath.branch_path() / basename;
+        if(fs::exists(filepath))
+            {
+            fatal_error()
+                << "Cannot create unique file path from file name '"
+                << original_filepath.string()
+                << "' with extension '"
+                << extension
+                << "'."
+                << LMI_FLUSH
+                ;
+            }
+        }
+    return filepath;
+}
+
---
+++ path_utility_test.cpp
@@ -0,0 +1,141 @@
+// Path utilities--unit test.
+//
+// Copyright (C) 2005 Gregory W. Chicares.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+// http://savannah.nongnu.org/projects/lmi
+// email: <chicares@cox.net>
+// snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
+
+// $Id: path_utility_test.cpp,v 1.1 2005-04-29 10:09:59 chicares Exp $
+
+#ifdef __BORLANDC__
+#   include "pchfile.hpp"
+#   pragma hdrstop
+#endif // __BORLANDC__
+
+#include "path_utility.hpp"
+
+#include "platform_dependent.hpp" // access()
+
+#define BOOST_INCLUDE_MAIN
+#include "test_tools.hpp"
+
+#include <boost/filesystem/exception.hpp>
+#include <boost/filesystem/fstream.hpp>
+
+#include <fstream>
+
+namespace
+{
+/// To make sure a file exists, it's necessary to write something to
+/// it: C99 7.19.3/4, last sentence.
+
+    void write_dummy_file(char const* s)
+    {
+        std::ofstream ofs(s);
+        ofs << s;
+    }
+
+    void write_dummy_file(fs::path p)
+    {
+        fs::ofstream ofs(p);
+        ofs << p.string();
+    }
+}
+
+void test_normal_operation()
+{
+    // These tests would fail if read-only files with the following
+    // names already exist.
+
+    char const* p = "/tmp/eraseme.0";
+    char const* q = "/tmp/eraseme.xyzzy";
+
+    // Assume directory /tmp/ exists: the makefile provided ensures
+    // that, even on platforms that don't always provide it.
+
+    // Don't test the return codes here. These files probably don't
+    // exist, in which case C99 7.19.4.1 doesn't clearly prescribe
+    // the semantics of remove().
+    std::remove(p);
+    std::remove(q);
+
+    write_dummy_file(p);
+    BOOST_TEST_EQUAL(0, access(p, R_OK));
+
+    fs::path path0 = unique_filepath(fs::path(p), ".xyzzy");
+    BOOST_TEST_EQUAL(path0.string(), q);
+    write_dummy_file(path0);
+    BOOST_TEST_EQUAL(0, access(path0.string().c_str(), R_OK));
+
+    fs::path path1 = unique_filepath(fs::path(p), ".xyzzy");
+    BOOST_TEST_EQUAL(path1.string(), q);
+    write_dummy_file(path1);
+    BOOST_TEST_EQUAL(0, access(path1.string().c_str(), R_OK));
+
+    // Open a file for writing, and leave it open, preventing it from
+    // being erased and therefore forcing unique_filepath() to use a
+    // different name. This behavior isn't guaranteed on toy OS's.
+    fs::ofstream keep_open
+        (path1
+        ,std::ios_base::out | std::ios_base::ate | std::ios_base::app
+        );
+
+    fs::path path2 = unique_filepath(fs::path(p), ".xyzzy");
+    BOOST_TEST_UNEQUAL(path2.string(), q);
+    write_dummy_file(path2);
+    BOOST_TEST_EQUAL(0, access(path2.string().c_str(), R_OK));
+
+    // There's no easy way to test that unique_filepath() throws the
+    // intended exception if it's reinvoked more rapidly that its
+    // timestamp changes.
+
+    keep_open.close();
+
+    BOOST_TEST(0 == std::remove(p));
+    BOOST_TEST(0 == std::remove(q));
+    BOOST_TEST(0 == std::remove(path2.string().c_str()));
+}
+
+void test_ludicrous_filenames()
+{
+    fs::path path0 = unique_filepath(fs::path("eraseme1"), "");
+    BOOST_TEST_EQUAL(path0.string(), "eraseme1");
+
+    fs::path path1 = unique_filepath(fs::path("eraseme2"), "");
+    BOOST_TEST_EQUAL(path1.string(), "eraseme2");
+
+    // "" isn't likely to be a valid filename, but as a path, it's
+    // valid: the boost library's default path ctor constructs an
+    // empty path.
+
+    fs::path path2 = unique_filepath(fs::path(""), "");
+    BOOST_TEST_EQUAL(path2.string(), "");
+
+    BOOST_TEST_THROW
+        (unique_filepath(fs::path(".."), "..")
+        ,fs::filesystem_error, ""
+        );
+}
+
+int test_main(int, char*[])
+{
+    test_normal_operation();
+    test_ludicrous_filenames();
+
+    return EXIT_SUCCESS;
+}
+
