9849719bb202f16abcd71a5c972060ececc8382a
Gregory W. Chicares <chicares@sbcglobal.net>
2005-05-10 11:17:22 +0000

    Improve documentation

--- comma_punct.hpp
+++ comma_punct.hpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: comma_punct.hpp,v 1.1 2005-05-07 16:17:40 chicares Exp $
+// $Id: comma_punct.hpp,v 1.2 2005-05-10 11:17:22 chicares Exp $
 
 #ifndef comma_punct_hpp
 #define comma_punct_hpp
@@ -33,7 +33,9 @@
 // user-configurable locale, e.g.
 //   setlocale(LC_ALL, "En_US");
 // as a basis for customization, though perhaps any national locale
-// would require further customization. 
+// would require further customization. However, this would appear not
+// to work as of 2005-05-09 with gcc on the msw platform:
+//   http://sf.net/mailarchive/message.php?msg_id=11704377
 
 class comma_punct
     :public std::numpunct<char>
