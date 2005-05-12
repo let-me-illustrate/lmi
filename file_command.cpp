60c179544d8881fff7c3ccc44fe14423d56a2997
Greg Chicares <gchicares@massmutual.com>
2005-05-12 15:46:43 +0000

    Run a command against a file, respecting its extension
    git-svn-id: svn://svn.sv.gnu.org/lmi/lmi/trunk@458 8995fb05-a277-41b4-a4d2-e84fd280a9a6

---
+++ file_command.cpp
@@ -0,0 +1,38 @@
+// Run a command against a file, respecting its extension.
+//
+// Copyright (C) 2005 Gregory W. Chicares.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+// http://savannah.nongnu.org/projects/lmi
+// email: <chicares@cox.net>
+// snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
+
+// $Id: file_command.cpp,v 1.1 2005-05-12 15:46:43 chicares Exp $
+
+#ifdef __BORLANDC__
+#   include "pchfile.hpp"
+#   pragma hdrstop
+#endif // __BORLANDC__
+
+#include "file_command.hpp"
+
+callback<file_command_fp_type> file_command_callback;
+
+bool file_command_initialize(file_command_fp_type f)
+{
+    file_command_callback.initialize(f);
+    return true;
+}
+
---
+++ file_command_cli.cpp
@@ -0,0 +1,41 @@
+// Run file command--command-line interface.
+//
+// Copyright (C) 2005 Gregory W. Chicares.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+// http://savannah.nongnu.org/projects/lmi
+// email: <chicares@cox.net>
+// snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
+
+// $Id: file_command_cli.cpp,v 1.1 2005-05-12 15:46:43 chicares Exp $
+
+#ifdef __BORLANDC__
+#   include "pchfile.hpp"
+#   pragma hdrstop
+#endif // __BORLANDC__
+
+#include "file_command.hpp"
+
+namespace
+{
+void concrete_file_command
+    (std::string const& file
+    ,std::string const& action
+    )
+{}
+
+volatile bool ensure_setup = file_command_initialize(concrete_file_command);
+} // Unnamed namespace.
+
