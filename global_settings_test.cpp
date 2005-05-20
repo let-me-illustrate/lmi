e8dc2c60366cb84d987910ca2e810218a0a52895
Greg Chicares <gchicares@massmutual.com>
2005-05-20 16:34:10 +0000

    Add global-settings unit test
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/lmi/lmi/trunk@494 8995fb05-a277-41b4-a4d2-e84fd280a9a6

---
+++ global_settings_test.cpp
@@ -0,0 +1,43 @@
+// Global settings--unit test.
+//
+// Copyright (C) 2005 Gregory W. Chicares.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+// http://savannah.nongnu.org/projects/lmi
+// email: <chicares@cox.net>
+// snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
+
+// $Id: global_settings_test.cpp,v 1.1 2005-05-20 16:34:10 chicares Exp $
+
+#ifdef __BORLANDC__
+#   include "pchfile.hpp"
+#   pragma hdrstop
+#endif // __BORLANDC__
+
+#include "global_settings.hpp"
+
+#define BOOST_INCLUDE_MAIN
+#include "test_tools.hpp"
+
+#include <boost/filesystem/path.hpp>
+#include <boost/filesystem/operations.hpp>
+
+/// Just an empty shell for now.
+
+int test_main(int, char*[])
+{
+    return 0;
+}
+
