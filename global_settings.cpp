a2122b2e31622e6c3c709e0c9ec1d0a67288a6f6
Gregory W. Chicares <chicares@sbcglobal.net>
2005-06-21 05:26:39 +0000

    Refactor

--- global_settings.cpp
+++ global_settings.cpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: global_settings.cpp,v 1.2 2005-06-21 03:21:56 chicares Exp $
+// $Id: global_settings.cpp,v 1.3 2005-06-21 05:26:39 chicares Exp $
 
 #ifdef __BORLANDC__
 #   include "pchfile.hpp"
@@ -28,30 +28,112 @@
 
 #include "global_settings.hpp"
 
+#if 0
+// Code copied from elsewhere that belongs here.
+
+    // TODO ?? Path validation belongs in class global_settings.
+    if(0 == data_directory.size())
+        {
+        data_directory = ".";
+        }
+    fs::path path(data_directory);
+    if(!fs::exists(path) || !fs::is_directory(path))
+        {
+        hobsons_choice()
+            << "Data directory '"
+            << path.string()
+            << "' not found."
+            << LMI_FLUSH
+            ;
+        }
+[Not yet done]
+
+Elsewhere was formerly found code like
+                global_settings::instance().set_ash_nazg(true);
+                global_settings::instance().set_mellon(true);
+but ash_nazg should imply mellon, and the place to do that is here.
+[Done]
+#endif // 0
+
 // Initialize directory strings to ".", not an empty string. Reason:
 // objects of the boost filesystem library's path class are created
 // from these strings, and that class deliberately rejects empty
 // strings.
 
 global_settings::global_settings()
-    :mellon                    (false)
-    ,ash_nazg                  (false)
-    ,special_output            (false)
-    ,data_directory            (".")
-    ,regression_test_directory (".")
+    :mellon_                    (false)
+    ,ash_nazg_                  (false)
+    ,custom_io_0_               (false)
+    ,data_directory_            (".")
+    ,regression_test_directory_ (".")
 {}
 
 global_settings::~global_settings()
 {}
 
-bool global_settings::regression_testing()
-{
-    return regression_test_directory.empty();
-}
-
 global_settings& global_settings::instance()
 {
     static global_settings z;
     return z;
+}
+
+bool global_settings::regression_testing() const
+{
+    return regression_test_directory_.empty();
+}
+
+void global_settings::set_mellon(bool b)
+{
+    mellon_ = b;
+}
+
+void global_settings::set_ash_nazg(bool b)
+{
+    ash_nazg_ = b;
+    if(b)
+        {
+        // 'ash_nazg' implies 'mellon'.
+        mellon_ = b;
+        }
+}
+
+void global_settings::set_custom_io_0(bool b)
+{
+    custom_io_0_ = b;
+}
+
+void global_settings::set_data_directory(std::string const& s)
+{
+    data_directory_ = s;
+}
+
+void global_settings::set_regression_test_directory(std::string const& s)
+{
+    regression_test_directory_ = s;
+}
+
+bool global_settings::mellon() const
+{
+    return mellon_;
+}
+
+bool global_settings::ash_nazg() const
+{
+    return ash_nazg_;
+}
+
+bool global_settings::custom_io_0() const
+{
+    return custom_io_0_;
+}
+
+std::string const& global_settings::data_directory() const
+{
+    return data_directory_;
+}
+
+std::string const& global_settings::regression_test_directory() const
+{
+    return regression_test_directory_;
 }
 
