d4a949993f4c37f440d429695b221e45fb82ba29
Greg Chicares <gchicares@massmutual.com>
2005-08-04 02:23:08 +0000

    Demonstrate a suspected wx defect
    git-svn-id: svn://svn.sv.gnu.org/lmi/lmi/trunk@724 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- text_view.cpp
+++ text_view.cpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: text_view.cpp,v 1.3 2005-04-22 02:21:21 chicares Exp $
+// $Id: text_view.cpp,v 1.4 2005-08-04 02:20:13 chicares Exp $
 
 // This is a derived work based on wxWindows file
 //   samples/docvwmdi/view.cpp (C) 1998 Julian Smart and Markus Holzem
@@ -35,13 +35,24 @@
 #endif
 
 #include "text_view.hpp"
+
+#include "alert.hpp"
 #include "wx_new.hpp"
 
 #include <wx/icon.h>
+#include <wx/log.h>
 #include <wx/menu.h>
 #include <wx/textctrl.h>
+#include <wx/xrc/xmlres.h>
+
+#include <stdexcept>
 
 IMPLEMENT_DYNAMIC_CLASS(TextEditView, ViewEx)
+
+BEGIN_EVENT_TABLE(TextEditView, ViewEx)
+    EVT_MENU(XRCID("test_alert_streams"),TextEditView::OnTestAlertStreams)
+    EVT_MENU(XRCID("test_exception"    ),TextEditView::OnTestException)
+END_EVENT_TABLE()
 
 TextEditView::TextEditView()
     :ViewEx      ()
@@ -73,5 +84,20 @@ wxIcon TextEditView::Icon() const
 wxMenuBar* TextEditView::MenuBar() const
 {
     return MenuBarFromXmlResource("text_view_menu");
+}
+
+void TextEditView::OnTestAlertStreams(wxCommandEvent&)
+{
+    status()         << "Test status()"         << LMI_FLUSH;
+    warning()        << "Test warning()"        << LMI_FLUSH;
+    hobsons_choice() << "Test hobsons_choice()" << LMI_FLUSH;
+    fatal_error()    << "Test fatal_error()"    << LMI_FLUSH;
+    wxLogError("Calling FlushActive()");
+    wxLog::FlushActive();
+}
+
+void TextEditView::OnTestException(wxCommandEvent&)
+{
+    throw std::runtime_error("Test std::runtime_error");
 }
 
