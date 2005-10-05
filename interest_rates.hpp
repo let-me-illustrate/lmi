88a192645dc01e77fddf8be325b2ae34faebf515
Greg Chicares <gchicares@massmutual.com>
2005-10-05 17:07:52 +0000

    Refactor illustration-reg applicability
    git-svn-id: svn://svn.sv.gnu.org/lmi/lmi/trunk@878 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- interest_rates.hpp
+++ interest_rates.hpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: interest_rates.hpp,v 1.2 2005-07-18 03:33:46 chicares Exp $
+// $Id: interest_rates.hpp,v 1.3 2005-10-05 17:07:52 chicares Exp $
 
 #ifndef interest_rates_hpp
 #define interest_rates_hpp
@@ -217,7 +217,6 @@ class InterestRates
     void Initialize7702Rates();
 
     int                Length_;
-    e_ledger_type      LedgerType_;
     round_to<double>   RoundIntRate_;
     round_to<double>   Round7702Rate_;
 
