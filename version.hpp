4ccd55b662a42030d0fca1dd34dccd441c9753c8
Gregory W. Chicares <chicares@sbcglobal.net>
2005-10-21 17:06:38 +0000

    Mark release candidate

--- version.hpp
+++ version.hpp
@@ -17,6 +17,6 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: version.hpp,v 1.24 2005-08-21 12:00:25 chicares Exp $
+// $Id: version.hpp,v 1.25 2005-10-21 17:06:38 chicares Exp $
 
-#define LMI_VERSION "20050820T2255Z"
+#define LMI_VERSION "20051021T0318Z"
