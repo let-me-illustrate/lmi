e0eda3b8e857a9f2f7c58da4d045317fb92f2694
Vadim Zeitlin <vadim@tt-solutions.com>
2005-10-31 01:44:31 +0000

    first draft of configure-based build system (tested under Linux only with g++ 3.3 and 4.0)
    git-svn-id: svn://svn.sv.gnu.org/lmi/lmi/trunk@930 8995fb05-a277-41b4-a4d2-e84fd280a9a6

---
+++ autogen.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# Script to execute to initialize lmi build system after checking out pristine
+# sources from a version control system: this script creates all generated
+# files which are needed for the build but not stored under version control.
+#
+# Copyright (C) 2005 Vadim Zeitlin.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# http://savannah.nongnu.org/projects/lmi
+# email: <chicares@cox.net>
+# snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
+
+# $Id: autogen.sh,v 1.1 2005-10-31 01:44:31 zeitlin Exp $
+
+if [ ! -f configure.ac -o ! -f Makefile.am -o ! -f lmi.xpm ]; then
+    echo "Please run this script from the lmi source directory."
+    exit 2
+fi
+
+# use --foreign with automake because we lack standard GNU NEWS and AUTHOR
+# files, if they're added we can "upgrade" to (default) GNU strictness
+echo "Setting up build system for lmi:"
+echo " - aclocal " && aclocal ${wx+-I $wx} && \
+echo " - libtoolize " && libtoolize --automake && \
+echo " - autoconf " && autoconf && \
+echo " - autoheader " && autoheader && \
+echo " - automake " && automake -a --foreign && \
+echo " - configure " && ./configure "$@" && \
+echo "Build setup successful, type \"make\" to build lmi now."
+exit 0
+
+echo "Automatic build files setup failed!"
+
+exit 1
