0510bb952fbfe8311a0a4be96afb2b1131f8a549
Gregory W. Chicares <chicares@sbcglobal.net>
2005-12-07 16:49:10 +0000

    Revise documentation

--- transferor.hpp
+++ transferor.hpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: transferor.hpp,v 1.2 2005-03-11 03:39:32 chicares Exp $
+// $Id: transferor.hpp,v 1.3 2005-12-07 16:49:10 chicares Exp $
 
 // Acknowledgment
 
@@ -29,79 +29,10 @@
 // but is implemented differently and has different goals, so any
 // problems introduced here don't reflect on Kevin Smith's reputation.
 
-// Design notes
-
-// Class Transferor transfers data between standard controls and
-// std::strings, whereas class wxGenericValidator transfers data
-// between standard controls and control-specific datatypes:
-//
-//   wxGenericValidator datatypes
-// bool*       wxCheckBox, wxRadioButton
-// wxString*   wxButton, wxComboBox, wxStaticText, wxTextCtrl
-// int*        wxChoice, wxGauge, wxRadioBox, wxSpinButton, wxSpinCtrl
-//               and, oddly enough, wxScrollBar
-// wxArrayInt* wxCheckListBox, wxListBox
-//
-// The wx 'validator' classes are thus designed to bind directly to
-// variables of multifarious wx datatypes, but this class is designed
-// to bind to std::strings, which can it turn be bound to any other
-// type that may be desired using only standard C++. This class's
-// implementation would be trivial if wx provided member functions
-//   wxControl::StringValue(std::string const&);
-//   std::string wxControl::GetStringValue() const;
-// but the library's interface is less simple, e.g.
-//   wxButton       *str = GetLabel()             SetLabel(*str)
-//   wxCheckBox     *bool = GetValue()            SetValue(*bool)
-//   wxCheckListBox *str = GetStringSelection()   SetStringSelection(*str)
-//   wxChoice       *str = GetStringSelection()   SetStringSelection(*str)
-//   wxComboBox     *int = GetSelection()         SetSelection(*int)
-//                  *str = GetValue()             SetStringSelection(*str)
-//                                                SetValue(*str)
-//   wxGauge        *int = GetValue()             SetValue(*int)
-//   wxListBox      *str = GetStringSelection()   SetStringSelection(*str)
-//                  if(Selected(i)) array->Add(i) SetSelection(array->Item(i))
-//   wxRadioBox     *int = GetSelection()         SetSelection(*int)
-//                  *str = GetStringSelection()   SetStringSelection(*str)
-//   wxRadioButton  *int = GetValue()             SetValue(*int)
-//   wxScrollBar    *int = GetThumbPosition       SetThumbPosition(*int)
-//   wxSlider       *int = GetValue()             SetValue(*int)
-//   wxSpinButton   *int = GetValue()             SetValue(*int)
-//   wxSpinCtrl     *int = GetValue()             SetValue(*int)
-//   wxStaticText   *str = GetLabel()             SetLabel(*str)
-//   wxTextCtrl     *str = GetValue()             SetValue(*str)
-
-// The base class's name is confusing: it only transfers data, and
-// validates nothing. Its Validate() member function simply returns
-// 'false' in all cases, apparently to force users to derive from it.
-// This class overrides Validate() to return 'true' in all cases,
-// because wx uses 'false' to signal failure. Even though this
-// overridden function may be called, it doesn't actually validate
-// anything--this design contemplates performing validation on the
-// strings that controls are transferred to.
-
-// This class is less general than wx 'validators' in that it permits
-// only single selections with controls that might allow multiple
-// selections. Multiple-selection controls are not used in the
-// application this class was designed for, whose users find such
-// controls confusing.
-
-// Technical notes
-
-// The wx documentation says "All validator classes must implement the
-// Clone function, which returns an identical copy of itself." The
-// base class is Noncopyable, although it has a nonvirtual Copy()
-// function that it is apparently safe to ignore here.
-
-// Boost coding guideline 15.5 warns that reference members preclude
-// assignment, but this class is Nonassignable, and nonreseatable
-// semantics are exactly appropriate for the string bound to.
-
 #ifndef transferor_hpp
 #define transferor_hpp
 
 #include "config.hpp"
-
-#include "obstruct_slicing.hpp"
 
 #include <boost/utility.hpp>
 
@@ -114,25 +45,93 @@ enum transfer_direction
     ,from_string_to_control
     };
 
+/// Design notes
+///
+/// Class Transferor transfers data between standard controls and
+/// std::strings, whereas class wxGenericValidator transfers data
+/// between standard controls and control-specific datatypes:
+///
+///   wxGenericValidator datatypes
+/// bool*       wxCheckBox, wxRadioButton
+/// wxString*   wxButton, wxComboBox, wxStaticText, wxTextCtrl
+/// int*        wxChoice, wxGauge, wxRadioBox, wxSpinButton, wxSpinCtrl
+///               and, oddly enough, wxScrollBar
+/// wxArrayInt* wxCheckListBox, wxListBox
+///
+/// The wx 'validator' classes are thus designed to bind directly to
+/// variables of multifarious wx datatypes, but this class is designed
+/// to bind to std::strings, which can it turn be bound to any other
+/// type that may be desired using only standard C++. This class's
+/// implementation would be trivial if wx provided member functions
+///   wxControl::StringValue(std::string const&);
+///   std::string wxControl::GetStringValue() const;
+/// but the library's interface is less simple, e.g.
+///   wxButton       *str = GetLabel()             SetLabel(*str)
+///   wxCheckBox     *bool = GetValue()            SetValue(*bool)
+///   wxCheckListBox *str = GetStringSelection()   SetStringSelection(*str)
+///   wxChoice       *str = GetStringSelection()   SetStringSelection(*str)
+///   wxComboBox     *int = GetSelection()         SetSelection(*int)
+///                  *str = GetValue()             SetStringSelection(*str)
+///                                                SetValue(*str)
+///   wxGauge        *int = GetValue()             SetValue(*int)
+///   wxListBox      *str = GetStringSelection()   SetStringSelection(*str)
+///                  if(Selected(i)) array->Add(i) SetSelection(array->Item(i))
+///   wxRadioBox     *int = GetSelection()         SetSelection(*int)
+///                  *str = GetStringSelection()   SetStringSelection(*str)
+///   wxRadioButton  *int = GetValue()             SetValue(*int)
+///   wxScrollBar    *int = GetThumbPosition       SetThumbPosition(*int)
+///   wxSlider       *int = GetValue()             SetValue(*int)
+///   wxSpinButton   *int = GetValue()             SetValue(*int)
+///   wxSpinCtrl     *int = GetValue()             SetValue(*int)
+///   wxStaticText   *str = GetLabel()             SetLabel(*str)
+///   wxTextCtrl     *str = GetValue()             SetValue(*str)
+///
+/// The base class's name is confusing: it only transfers data, and
+/// validates nothing. Its Validate() member function simply returns
+/// 'false' in all cases, apparently to force users to derive from it.
+/// This class overrides Validate() to return 'true' in all cases,
+/// because wx uses 'false' to signal failure. Even though this
+/// overridden function may be called, it doesn't actually validate
+/// anything--this design contemplates performing validation on the
+/// strings that controls are transferred to.
+///
+/// This class is less general than wx 'validators' in that it permits
+/// only single selections with controls that might allow multiple
+/// selections. Multiple-selection controls are not used in the
+/// application this class was designed for, whose users find such
+/// controls confusing.
+///
+/// Technical notes
+///
+/// The wx documentation says "All validator classes must implement the
+/// Clone function, which returns an identical copy of itself." The
+/// base class is Noncopyable, although it has a nonvirtual Copy()
+/// function that it is apparently safe to ignore here.
+///
+/// Boost coding guideline 15.5 warns that reference members preclude
+/// assignment, but this class is Nonassignable, and nonreseatable
+/// semantics are exactly appropriate for the string bound to.
+///
+/// Base-class function Validate() returns false by default. It is
+/// overridden here to return true instead.
+
 class Transferor
     :public wxValidator
     ,private boost::noncopyable
-    ,virtual private obstruct_slicing<Transferor>
 {
   public:
     Transferor(std::string& data, std::string const& name);
     virtual ~Transferor();
 
     // wxValidator overrides.
+    virtual wxObject* Clone() const;
     virtual bool TransferFromWindow();
     virtual bool TransferToWindow();
     virtual bool Validate(wxWindow*);
-    virtual wxObject* Clone() const;
 
     std::string const& name() const;
 
   private:
-    // TODO ?? Use a lowercase name.
     bool PerformTransfer(transfer_direction);
 
     std::string& data_;
