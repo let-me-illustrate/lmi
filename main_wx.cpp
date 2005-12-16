066720dca39a68bb30b69995eb26285dfb051888
Gregory W. Chicares <chicares@sbcglobal.net>
2005-12-16 23:39:40 +0000

    Simplify

--- main_wx.cpp
+++ main_wx.cpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: main_wx.cpp,v 1.36 2005-12-15 15:40:12 chicares Exp $
+// $Id: main_wx.cpp,v 1.37 2005-12-16 23:39:40 chicares Exp $
 
 // Portions of this file are derived from wxWindows files
 //   samples/docvwmdi/docview.cpp (C) 1998 Julian Smart and Markus Holzem
@@ -187,7 +187,10 @@ int WINAPI WinMain
         result = wxEntry(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
 #endif // __WXMSW__ defined.
         }
-    LMI_CATCH_AND_REPORT_EXCEPTION;
+    catch(...)
+        {
+        report_exception();
+        }
 
     validate_fenv();
 
@@ -393,7 +396,10 @@ bool Skeleton::OnExceptionInMainLoop()
         // 'monolithic' wx shared library.
         return wxAppConsole::OnExceptionInMainLoop();
         }
-    LMI_CATCH_AND_REPORT_EXCEPTION;
+    catch(...)
+        {
+        report_exception();
+        }
     return true;
 }
 
@@ -489,7 +495,10 @@ bool Skeleton::OnInit()
             wxPostEvent(frame_, event);
             }
         }
-    LMI_CATCH_AND_REPORT_EXCEPTION;
+    catch(...)
+        {
+        report_exception();
+        }
     return true;
 }
 
