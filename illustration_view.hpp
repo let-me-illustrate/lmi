90713aab1602ee42a86269380c09d10865fd6b56
Greg Chicares <gchicares@massmutual.com>
2005-12-16 23:39:35 +0000

    Include a header only where required
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/lmi/lmi/trunk@1181 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- illustration_view.hpp
+++ illustration_view.hpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: illustration_view.hpp,v 1.11 2005-12-01 04:06:34 chicares Exp $
+// $Id: illustration_view.hpp,v 1.12 2005-12-16 23:39:35 chicares Exp $
 
 // This is a derived work based on wxWindows file
 //   samples/docvwmdi/view.cpp (C) 1998 Julian Smart and Markus Holzem
@@ -36,7 +36,6 @@
 
 #include "view_ex.hpp"
 
-#include "illustration_document.hpp"
 #include "obstruct_slicing.hpp"
 
 #include <boost/shared_ptr.hpp>
@@ -46,6 +45,7 @@
 
 #include <string>
 
+class IllustrationDocument;
 class Input;
 class Ledger;
 class WXDLLEXPORT wxHtmlWindow;
