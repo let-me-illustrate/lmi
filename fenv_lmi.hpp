6bc87a20cd607a1eb96b42e2b547c2f2534752d0
Greg Chicares <gchicares@massmutual.com>
2005-12-27 15:36:50 +0000

    Rewrite floating-point-environment management, adding unit tests
    git-svn-id: svn://svn.sv.gnu.org/lmi/lmi/trunk@1201 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- fenv_lmi.hpp
+++ fenv_lmi.hpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: fenv_lmi.hpp,v 1.5 2005-12-22 13:59:49 chicares Exp $
+// $Id: fenv_lmi.hpp,v 1.6 2005-12-27 15:36:50 chicares Exp $
 
 // Manage the floating-point environment, using C99 7.6 facilities
 // where available. Otherwise, use compiler- and platform-specific
@@ -33,12 +33,34 @@
 
 #include "so_attributes.hpp"
 
-// Initialize floating-point environment to reasonable settings.
-extern "C" void LMI_SO initialize_fpu();
+#ifdef LMI_X86
+#   include "fenv_lmi_x86.hpp"
+#else  // Unknown compiler or platform.
+#   error Unknown compiler or platform. Please contribute an implementation.
+#endif // Unknown compiler or platform.
+
+#if defined __STDC_IEC_559__ || defined __MINGW32__
+// In case the C++ compiler supports C99 7.6 facilities, assume that
+// it defines __STDC_IEC_559__ (except that MinGW supports some such
+// facilities but defines no such macro), and puts prototypes in
+// <fenv.h> but not in namespace std.
+#   include <fenv.h>
+#   if defined __GNUC__ && LMI_GCC_VERSION <= 40100
+// As of 2005-04-08, the gcc manual here:
+//   http://gcc.gnu.org/onlinedocs/gcc/Floating-point-implementation.html
+// which "corresponds to GCC version 4.1.0" says "This pragma is not
+// implemented".
+#   else  // Pragma STDC FENV_ACCESS implemented.
+#       pragma STDC FENV_ACCESS ON
+#   endif // Pragma STDC FENV_ACCESS implemented.
+#endif // defined __STDC_IEC_559__ || defined __MINGW32__
+
+// Initialize floating-point environment to lmi default settings.
+void LMI_SO initialize_fpu();
 
 // Make sure current floating-point environment matches initial
-// settings; throw an exception if it doesn't.
-extern "C" void LMI_SO validate_fenv();
+// settings; display a message and return 'false' if it doesn't.
+bool LMI_SO validate_fenv();
 
 #endif // fenv_lmi_hpp
 
