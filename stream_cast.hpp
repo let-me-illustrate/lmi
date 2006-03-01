b91ef8dbcb85d173f80f49707b139e959f813571
Greg Chicares <gchicares@massmutual.com>
2006-03-01 15:52:17 +0000

    Rewrite a conditional to increase maintainability
    git-svn-id: svn://svn.sv.gnu.org/lmi/lmi/trunk@1349 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- stream_cast.hpp
+++ stream_cast.hpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: stream_cast.hpp,v 1.9 2006-02-28 13:35:18 chicares Exp $
+// $Id: stream_cast.hpp,v 1.10 2006-03-01 15:52:17 chicares Exp $
 
 // This is a derived work based on boost::lexical_cast, which bears
 // the following copyright and permissions notice:
@@ -132,7 +132,7 @@ To stream_cast(From from, To = To())
         {
         err << "Unconverted data remains ";
         }
-#endif // Not defined LMI_COMO_WITH_MINGW .
+#endif // !defined LMI_COMO_WITH_MINGW
     else
         {
         ; // Nothing left to do.
