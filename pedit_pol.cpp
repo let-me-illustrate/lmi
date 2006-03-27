d7af3ca5b700a6bdfd8f4c1e0f001a60f2a44a33
Evgeniy Tarassov <etarassov@tt-solutions.com>
2006-03-27 13:02:05 +0000

    Add lmi copyright header
    git-svn-id: svn://svn.sv.gnu.org/lmi/lmi/branches/mdgrid_branch@1444 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- pedit_pol.cpp
+++ pedit_pol.cpp
@@ -1,3 +1,26 @@
+// Product editor part for pol file type.
+//
+// Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005 Gregory W. Chicares.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+// http://savannah.nongnu.org/projects/lmi
+// email: <chicares@cox.net>
+// snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
+
+// $Id: pedit_pol.cpp,v 1.1.2.3 2006-03-27 13:02:05 etarassov Exp $
+
 #include "pedit_pol.hpp"
 
 #include <memory>
