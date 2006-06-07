2c1e7cfb023da1c2ef7f7bebdc133d7842dc3113
Wendy Boutin <wboutin@massmutual.com>
2006-06-07 17:07:43 +0000

    Improve conformance
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/lmi/lmi/trunk@1588 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- rtti_lmi.hpp
+++ rtti_lmi.hpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id:
+// $Id: rtti_lmi.hpp,v 1.2 2006-06-07 17:07:43 wboutin Exp $
 
 #ifndef rtti_lmi_hpp
 #define rtti_lmi_hpp
