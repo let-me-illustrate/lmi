5ac884042f06fe169a098551d40fd00b5acda098
Greg Chicares <gchicares@massmutual.com>
2006-07-07 11:22:08 +0000

    Import Model-View-Controller files from skeleton trunk
    git-svn-id: svn://svn.sv.gnu.org/lmi/lmi/trunk@1752 8995fb05-a277-41b4-a4d2-e84fd280a9a6

---
+++ mvc_model.cpp
@@ -0,0 +1,180 @@
+// MVC Model base class.
+//
+// Copyright (C) 2004, 2005, 2006 Gregory W. Chicares.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+//
+// http://savannah.nongnu.org/projects/lmi
+// email: <chicares@cox.net>
+// snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
+
+// $Id: mvc_model.cpp,v 1.1 2006-07-07 11:22:08 chicares Exp $
+
+#ifdef __BORLANDC__
+#   include "pchfile.hpp"
+#   pragma hdrstop
+#endif // __BORLANDC__
+
+#include "mvc_model.hpp"
+
+#include "alert.hpp"
+#include "any_entity.hpp"
+
+namespace
+{
+void ComplainAboutAnyDiscrepancies
+    (MvcModel::StateType const& old_values
+    ,MvcModel::StateType const& new_values
+    ,std::string const&  description
+    )
+{
+    if(old_values == new_values)
+        {
+        return;
+        }
+
+    fatal_error() << description << '\n';
+    MvcModel::StateType::const_iterator i = old_values.begin();
+    MvcModel::StateType::const_iterator j = new_values.begin();
+    LMI_ASSERT(old_values.size() == new_values.size());
+    for(; i != old_values.end(); ++i, ++j)
+        {
+        LMI_ASSERT(i->first == j->first);
+        if(i->second != j->second)
+            {
+            fatal_error()
+                << "  "
+                << i->first
+                << " must change from '"
+                << i->second
+                << "' to '"
+                << j->second
+                << "'\n"
+                ;
+            }
+        }
+    fatal_error() << LMI_FLUSH;
+}
+} // Unnamed namespace.
+
+MvcModel::MvcModel()
+{
+}
+
+MvcModel::~MvcModel()
+{
+}
+
+datum_base const* MvcModel::BaseDatumPointer(std::string const& name) const
+{
+    return DoBaseDatumPointer(name);
+}
+
+any_entity& MvcModel::Entity(std::string const& name)
+{
+    return DoEntity(name);
+}
+
+any_entity const& MvcModel::Entity(std::string const& name) const
+{
+    return DoEntity(name);
+}
+
+MvcModel::NamesType const& MvcModel::Names() const
+{
+    return DoNames();
+}
+
+MvcModel::StateType MvcModel::State() const
+{
+    return DoState();
+}
+
+void MvcModel::Reconcile()
+{
+    StateType new_values = State();
+    StateType old_values;
+
+    bool okay = false;
+    int j = 0;
+    int const maximum_iterations = 100;
+
+    for(; !okay && j < maximum_iterations; ++j)
+        {
+        Harmonize();
+        Transmogrify();
+        old_values.swap(new_values);
+        new_values = State();
+        okay = new_values == old_values;
+        }
+
+    if(!okay)
+        {
+        fatal_error()
+            << "Unable to make Model consistent after "
+            << maximum_iterations
+            << " iterations."
+            << LMI_FLUSH
+            ;
+        }
+}
+
+void MvcModel::TestInitialConsistency()
+{
+    std::string description;
+    StateType old_values;
+    StateType new_values;
+
+    description = "Static initial values are inconsistent with rules:";
+    old_values = State();
+    Harmonize();
+    Transmogrify();
+    new_values = State();
+    ComplainAboutAnyDiscrepancies(old_values, new_values, description);
+
+    description = "Dynamic initial values are inconsistent with rules:";
+    CustomizeInitialValues();
+    old_values = State();
+    Harmonize();
+    Transmogrify();
+    new_values = State();
+    ComplainAboutAnyDiscrepancies(old_values, new_values, description);
+}
+
+void MvcModel::CustomizeInitialValues()
+{
+    DoCustomizeInitialValues();
+    Harmonize();
+    Transmogrify();
+}
+
+void MvcModel::Harmonize()
+{
+    StateType const old_values = State();
+    DoHarmonize();
+    StateType const new_values = State();
+    std::string description = "Harmonize() improperly forces values to change:";
+    ComplainAboutAnyDiscrepancies(old_values, new_values, description);
+}
+
+void MvcModel::Transmogrify()
+{
+    typedef NamesType::const_iterator ntci;
+    for(ntci i = Names().begin(); i != Names().end(); ++i)
+        {
+        DoEnforceRangeLimit(*i);
+        }
+    DoTransmogrify();
+}
+
