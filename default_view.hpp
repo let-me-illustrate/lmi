a808d788d66f0f55c6f2eb8af032e069ef0c44e2
Gregory W. Chicares <chicares@sbcglobal.net>
2006-07-10 17:59:18 +0000

    Implement a default MVC View

---
+++ default_view.hpp
@@ -0,0 +1,52 @@
+// Default MVC View for life-insurance illustrations.
+//
+// Copyright (C) 2006 Gregory W. Chicares.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+//
+// http://savannah.nongnu.org/projects/lmi
+// email: <chicares@cox.net>
+// snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
+
+// $Id: default_view.hpp,v 1.1 2006-07-10 17:59:18 chicares Exp $
+
+#ifndef default_view_hpp
+#define default_view_hpp
+
+#include "config.hpp"
+
+#include "mvc_view.hpp"
+
+/// Design notes for class DefaultView.
+///
+/// This concrete class implements a default MvcView designed for
+/// life-insurance illustrations.
+///
+/// Implicitly-declared special member functions do the right thing.
+
+class DefaultView
+    :public MvcView
+{
+  public:
+    DefaultView();
+    virtual ~DefaultView();
+
+  private:
+    virtual char const* DoBookControlName () const;
+    virtual char const* DoMainDialogName  () const;
+    virtual char const* DoResourceFileName() const;
+};
+
+#endif // default_view_hpp
+
