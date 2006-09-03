4995eff0f0b7343272d26613fbefcca4c35e8d5c
Greg Chicares <gchicares@massmutual.com>
2006-09-03 13:33:14 +0000

    Offer more supplemental-report columns
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/lmi/lmi/trunk@1957 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- mc_enum_types.cpp
+++ mc_enum_types.cpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: mc_enum_types.cpp,v 1.10 2006-01-29 13:52:00 chicares Exp $
+// $Id: mc_enum_types.cpp,v 1.11 2006-09-03 13:33:14 chicares Exp $
 
 #ifdef __BORLANDC__
 #   include "pchfile.hpp"
@@ -94,7 +94,7 @@ MC_DEFINE(part_mort_table,1)
 MC_DEFINE(premium_table,1)
 MC_DEFINE(from_point,4)
 MC_DEFINE(to_point,4)
-MC_DEFINE(report_column,34)
+MC_DEFINE(report_column,43)
 
 #undef MC_DEFINE
 
