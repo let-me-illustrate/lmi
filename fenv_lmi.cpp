c7292c3c4362c230e86080a5c3cbd7ebda390ea7
Greg Chicares <gchicares@massmutual.com>
2006-09-08 22:11:35 +0000

    Fix a GNU/Linux problem, noting two newly-observed defects
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/lmi/lmi/trunk@1982 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- fenv_lmi.cpp
+++ fenv_lmi.cpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: fenv_lmi.cpp,v 1.15 2006-02-21 01:39:15 chicares Exp $
+// $Id: fenv_lmi.cpp,v 1.16 2006-09-08 22:11:35 chicares Exp $
 
 #ifdef __BORLANDC__
 #   include "pchfile.hpp"
@@ -35,25 +35,25 @@
 
 void fenv_initialize()
 {
-#if defined LMI_X86
+#if 0 && defined __STDC_IEC_559__
+    // The facilities offered by C99's <fenv.h> are useful, but not
+    // sufficient: they require no standard facility to set hardware
+    // precision, although 7.6/9 provides for extensions like mingw's
+    //   fesetenv(FE_PC64_ENV);
+    // This block shows what could be accomplished in standard C.
+    fenv_t save_env;
+    feholdexcept(&save_env);
+    fesetround(FE_TONEAREST);
+#   if defined __MINGW32__
+    fesetenv(FE_PC64_ENV);
+#   else  // !defined __MINGW32__
+#       error Find a platform-specific way to set hardware precision.
+#   endif // !defined __MINGW32__
+#elif defined LMI_X86
     x87_control_word(default_x87_control_word());
 #else  // Unknown compiler or platform.
 #   error Unknown compiler or platform.
 #endif // Unknown compiler or platform.
-
-#if 0
-    // The facilities offered by C99's <fenv.h> are useful, but not
-    // sufficient: they require no standard facility to set hardware
-    // precision, although 7.6/9 provides for extensions like mingw's
-    // FE_PC64_ENV. This block shows what could be accomplished in
-    // standard C.
-#   if defined __STDC_IEC_559__
-    fenv_t save_env;
-    feholdexcept(&save_env);
-    fesetround(FE_TONEAREST);
-#       error Find a platform-specific way to set hardware precision.
-#   endif // __STDC_IEC_559__
-#endif // 0
 }
 
 e_ieee754_precision fenv_precision()
@@ -94,8 +94,15 @@ void fenv_precision(e_ieee754_precision precision_mode)
 
 e_ieee754_rounding fenv_rounding()
 {
-#if defined __STDC_IEC_559__
-    return fegetround(rounding_mode);
+#if 0 && defined __STDC_IEC_559__
+    int z = fegetround();
+    return
+          (FE_TONEAREST  == z) ? fe_tonearest
+        : (FE_DOWNWARD   == z) ? fe_downward
+        : (FE_UPWARD     == z) ? fe_upward
+        : (FE_TOWARDZERO == z) ? fe_towardzero
+        : throw std::runtime_error("Unknown rounding mode.");
+        ;
 #elif defined __BORLANDC__
     unsigned short int rc = (unsigned short int)(MCW_RC) & x87_control_word();
     return
@@ -103,7 +110,7 @@ e_ieee754_rounding fenv_rounding()
         : (RC_DOWN == rc) ? fe_downward
         : (RC_UP   == rc) ? fe_upward
         : (RC_CHOP == rc) ? fe_towardzero
-        : fenv_rounding_error()
+        : fenv_rounding_error() // TODO ?? Replace with throw-expression.
         ;
 #elif defined LMI_X86
     return intel_control_word(x87_control_word()).rc();;
@@ -113,7 +120,7 @@ e_ieee754_rounding fenv_rounding()
 }
 
 // This C99 function call
-//   fesetround(precision_mode);
+//   fesetround(rounding_mode);
 // is nearly equivalent, except that it takes an int argument.
 // It is not conditionally used here, because it seems unwise to
 // weaken the type safety C++ affords merely to implement this
@@ -121,13 +128,22 @@ e_ieee754_rounding fenv_rounding()
 //
 void fenv_rounding(e_ieee754_rounding rounding_mode)
 {
-#if defined __BORLANDC__
+#if 0 && defined __STDC_IEC_559__
+    int z =
+          (fe_tonearest  == rounding_mode) ? FE_TONEAREST
+        : (fe_downward   == rounding_mode) ? FE_DOWNWARD
+        : (fe_upward     == rounding_mode) ? FE_UPWARD
+        : (fe_towardzero == rounding_mode) ? FE_TOWARDZERO
+        : throw std::runtime_error("Unknown rounding mode.");
+        ;
+    fesetround(z);
+#elif defined __BORLANDC__
     unsigned short int z =
           (fe_tonearest  == rounding_mode) ? (unsigned short int)(RC_NEAR)
         : (fe_downward   == rounding_mode) ? (unsigned short int)(RC_DOWN)
         : (fe_upward     == rounding_mode) ? (unsigned short int)(RC_UP)
         : (fe_towardzero == rounding_mode) ? (unsigned short int)(RC_CHOP)
-        : fenv_rounding_error()
+        : fenv_rounding_error() // TODO ?? Replace with throw-expression.
         ;
     _control87(z, MCW_RC);
 #elif defined LMI_X86
