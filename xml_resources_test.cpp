26d8695d800421bde2760610b6458c56156a83ed
Greg Chicares <gchicares@massmutual.com>
2006-10-22 01:49:47 +0000

    Change cvs substitution mode to default '-kkv'
    git-svn-id: svn://svn.sv.gnu.org/lmi/lmi/branches/gnome-xml-branch@2079 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- xml_resources_test.cpp
+++ xml_resources_test.cpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: xml_resources_test.cpp,v 1.1.2.2 2006-10-22 01:30:32 chicares Exp $
+// $Id $
 
 #ifdef __BORLANDC__
 #   include "pchfile.hpp"
