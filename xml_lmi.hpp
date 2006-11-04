09cdba0e6c9bc87c253a269b688e67ea8b180b2b
Greg Chicares <gchicares@massmutual.com>
2006-11-04 19:49:15 +0000

    Begin to refactor xml parsing
    git-svn-id: svn://svn.sv.gnu.org/lmi/lmi/trunk@2193 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- xml_lmi.hpp
+++ xml_lmi.hpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: xml_lmi.hpp,v 1.2 2006-11-04 14:23:07 chicares Exp $
+// $Id: xml_lmi.hpp,v 1.3 2006-11-04 19:49:15 chicares Exp $
 
 #ifndef xml_lmi_hpp
 #define xml_lmi_hpp
@@ -28,15 +28,68 @@
 
 #include "xml_lmi_fwd.hpp"
 
+#include <boost/scoped_ptr.hpp>
+#include <boost/utility.hpp>
+
 #include <xmlwrapp/node.h> // xml::node::const_iterator
 
+#include <iosfwd>
+#include <string>
 #include <vector>
 
 /// Interface to xmlwrapp.
 
 namespace xml_lmi
 {
-    typedef std::vector<xml::node::const_iterator> ElementContainer;
+    typedef xml::node::const_iterator   ElementPointer;
+    typedef std::vector<ElementPointer> ElementContainer;
+
+    class dom_parser
+        :private boost::noncopyable
+    {
+        typedef xml::tree_parser DomParser;
+
+      public:
+        dom_parser(std::string const& filename);
+        dom_parser(std::istream&);
+        ~dom_parser();
+
+#if defined USING_CURRENT_XMLWRAPP
+        Document const& document() const;
+#endif // defined USING_CURRENT_XMLWRAPP
+        Element const& root_node(std::string const& expected_name) const;
+
+      private:
+        std::string                  error_context_;
+        boost::scoped_ptr<xml::init> initializer_;
+        boost::scoped_ptr<DomParser> parser_;
+    };
+
+    /// Create a container of pointers to an element's child elements.
+    /// The contents are notionally pointers, but actually iterators,
+    /// because that's the abstraction xmlwrapp provides; they seem to
+    /// function as incrementable smart pointers.
+    ///
+    /// If the second argument is specified, then only elements having
+    /// the given name are placed in the container.
+    ///
+    /// Only direct children are considered: children of child nodes
+    /// are not. Only child nodes that are elements are placed in the
+    /// container; other types of nodes are not.
+    ///
+    /// Precondition: No child element pointer returned by xml-library
+    /// calls is null.
+    ///
+    /// Postcondition: The container holds no null pointers.
+    ///
+    /// Throws: an exception, via fatal_error(), if a precondition is
+    /// violated, or if xml-library calls throw an exception derived
+    /// from std::exception.
+
+    ElementContainer child_elements
+        (Element const&
+        ,std::string const& name = std::string()
+        );
 } // namespace xml_lmi
 
 #endif //xml_lmi_hpp
