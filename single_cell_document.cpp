cf8416ba39b10bed54e15ef43879c6e9f99d8ced
Greg Chicares <gchicares@massmutual.com>
2006-11-04 19:49:15 +0000

    Begin to refactor xml parsing
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/lmi/lmi/trunk@2193 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- single_cell_document.cpp
+++ single_cell_document.cpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: single_cell_document.cpp,v 1.10 2006-11-04 14:27:06 chicares Exp $
+// $Id: single_cell_document.cpp,v 1.11 2006-11-04 19:49:15 chicares Exp $
 
 #ifdef __BORLANDC__
 #   include "pchfile.hpp"
@@ -30,14 +30,7 @@
 
 #include "alert.hpp"
 #include "inputillus.hpp"
-#include "istream_to_string.hpp"
 #include "xml_lmi.hpp"
-
-#ifdef USING_CURRENT_XMLWRAPP
-#   include <xmlwrapp/document.h>
-#endif // USING_CURRENT_XMLWRAPP defined.
-#include <xmlwrapp/init.h>
-#include <xmlwrapp/tree_parser.h>
 
 //============================================================================
 single_cell_document::single_cell_document()
@@ -58,30 +51,8 @@ single_cell_document::single_cell_document(IllusInputParms const& parms)
 single_cell_document::single_cell_document(std::string const& filename)
     :input_data_(new IllusInputParms(false))
 {
-    xml::init init;
-    xml::tree_parser parser(filename.c_str());
-    if(!parser)
-        {
-        throw std::runtime_error("Error parsing XML file.");
-        }
-
-#ifdef USING_CURRENT_XMLWRAPP
-    xml_lmi::Element& root = parser.get_document().get_root_node();
-#else // USING_CURRENT_XMLWRAPP not defined.
-    xml_lmi::Element& root = parser.get_root_node();
-#endif // USING_CURRENT_XMLWRAPP not defined.
-    if(xml_root_name() != root.get_name())
-        {
-        fatal_error()
-            << "XML node name is '"
-            << root.get_name()
-            << "' but '"
-            << xml_root_name()
-            << "' was expected."
-            << LMI_FLUSH
-            ;
-        }
-    parse(root);
+    xml_lmi::dom_parser parser(filename);
+    parse(parser.root_node(xml_root_name()));
 }
 
 //============================================================================
@@ -98,65 +69,21 @@ std::string single_cell_document::xml_root_name() const
 //============================================================================
 void single_cell_document::parse(xml_lmi::Element const& root)
 {
-// COMPILER !! Borland doesn't find operator==() in ns xml.
-#ifdef __BORLANDC__
-using namespace xml;
-#endif // __BORLANDC__
-
-// TODO ?? Seems like this should be a const iterator. OTOH, maybe
-// the streaming operators should take a node iterator rather than
-// a node as argument?
-    xml::node::const_iterator child = root.begin();
-    if(child->is_text())
-        {
-        // TODO ?? Explain what this does.
-        ++child;
-        }
-    if(!child->is_text())
-        {
-        *child >> *input_data_;
-        }
+    xml_lmi::ElementContainer const elements(xml_lmi::child_elements(root));
+    LMI_ASSERT(1 == elements.size());
+    *elements[0] >> *input_data_;
 }
 
 //============================================================================
 void single_cell_document::read(std::istream& is)
 {
-    // XMLWRAPP !! xmlwrapp-0.2.0 doesn't know about istreams yet, so
-    // read the istream into a std::string via a std::ostringstream
-    // and pass that to the xml::tree_parser ctor that takes a char* .
-
-    std::string s;
-    istream_to_string(is, s);
-    xml::init init;
-    xml::tree_parser parser(s.c_str(), 1 + s.size());
-    if(!parser)
-        {
-        throw std::runtime_error("Error parsing XML file.");
-        }
-
-#ifdef USING_CURRENT_XMLWRAPP
-    xml_lmi::Element& root = parser.get_document().get_root_node();
-#else // USING_CURRENT_XMLWRAPP not defined.
-    xml_lmi::Element& root = parser.get_root_node();
-#endif // USING_CURRENT_XMLWRAPP not defined.
-    if(xml_root_name() != root.get_name())
-        {
-        fatal_error()
-            << "XML node name is '"
-            << root.get_name()
-            << "' but '"
-            << xml_root_name()
-            << "' was expected."
-            << LMI_FLUSH
-            ;
-        }
-    parse(root);
+    xml_lmi::dom_parser parser(is);
+    parse(parser.root_node(xml_root_name()));
 }
 
 //============================================================================
 void single_cell_document::write(std::ostream& os)
 {
-    xml::init init;
     xml_lmi::Element root(xml_root_name().c_str());
     root << *input_data_;
     os << root;
--- xml_lmi.cpp
+++ xml_lmi.cpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: xml_lmi.cpp,v 1.2 2006-11-04 14:23:07 chicares Exp $
+// $Id: xml_lmi.cpp,v 1.3 2006-11-04 19:49:15 chicares Exp $
 
 #ifdef __BORLANDC__
 #   include "pchfile.hpp"
@@ -28,7 +28,219 @@
 
 #include "xml_lmi.hpp"
 
+#include "alert.hpp"
+#include "istream_to_string.hpp"
+
+#if defined USING_CURRENT_XMLWRAPP
+#   include <xmlwrapp/document.h>
+#endif // defined USING_CURRENT_XMLWRAPP
+#include <xmlwrapp/init.h>
+#include <xmlwrapp/tree_parser.h>
+
+#include <sstream>
+#include <stdexcept>
+
 namespace xml_lmi
 {
+/// Parse an xml file.
+///
+/// Precondition: argument names an accessible xml file.
+///
+/// Postconditions: member parser_ is a non-null pointer; the object
+/// it points to is valid in that its operator!() returns false.
+///
+/// Throws: std::runtime_error, via fatal_error(), if a precondition
+/// is violated, or if xml-library calls throw an exception derived
+/// from std::exception.
+
+xml_lmi::dom_parser::dom_parser(std::string const& filename)
+    :initializer_(new xml::init)
+{
+    try
+        {
+        error_context_ = "Unable to parse xml file '" + filename + "': ";
+        if(filename.empty())
+            {
+            throw std::runtime_error("File name is empty.");
+            }
+        parser_.reset(new DomParser(filename.c_str()));
+        if(0 == parser_.get())
+            {
+            throw std::runtime_error("Parser not initialized.");
+            }
+        if(true == parser_->operator!())
+            {
+#if defined USING_CURRENT_XMLWRAPP
+            throw std::runtime_error
+                ("Parser failed: " + parser_->get_error_message()
+                );
+#else  // !defined USING_CURRENT_XMLWRAPP
+            throw std::runtime_error("Parser failed.");
+#endif // !defined USING_CURRENT_XMLWRAPP
+            }
+        }
+    catch(std::exception const& e)
+        {
+        fatal_error() << error_context_ << e.what() << LMI_FLUSH;
+        }
+}
+
+/// Parse an xml stream.
+///
+/// Precondition: argument is an xml stream for which 0 == rdstate().
+///
+/// Postconditions: member parser_ is a non-null pointer; the object
+/// it points to is valid in that its operator!() returns false.
+///
+/// Throws: std::runtime_error, via fatal_error(), if a precondition
+/// is violated, or if xml-library calls throw an exception derived
+/// from std::exception.
+
+xml_lmi::dom_parser::dom_parser(std::istream& is)
+    :initializer_(new xml::init)
+{
+    try
+        {
+        // XMLWRAPP !! xmlwrapp has no such ctor as
+        //   xml::tree_parser(std::istream&)
+        // Therefore, read the std::istream into a std::string with
+        // istream_to_string(), and pass that to the xml::tree_parser
+        // ctor that takes a char* and a byte count.
+
+        error_context_ = "Unable to parse xml stream: ";
+        if(0 != is.rdstate())
+            {
+            throw std::runtime_error("Stream state is not 'good'.");
+            }
+        std::string s;
+        istream_to_string(is, s);
+        parser_.reset(new DomParser(s.c_str(), 1 + s.size()));
+        if(0 == parser_.get())
+            {
+            throw std::runtime_error("Parser not initialized.");
+            }
+        if(true == parser_->operator!())
+            {
+#if defined USING_CURRENT_XMLWRAPP
+            throw std::runtime_error
+                ("Parser failed: " + parser_->get_error_message()
+                );
+#else  // !defined USING_CURRENT_XMLWRAPP
+            throw std::runtime_error("Parser failed.");
+#endif // !defined USING_CURRENT_XMLWRAPP
+            }
+        }
+    catch(std::exception const& e)
+        {
+        fatal_error() << error_context_ << e.what() << LMI_FLUSH;
+        }
+}
+
+/// Throws: nothing unless member parser_'s destructor does.
+
+xml_lmi::dom_parser::~dom_parser()
+{}
+
+#if defined USING_CURRENT_XMLWRAPP
+/// Return the parsed document.
+///
+/// Preconditions: member parser_ has a document.
+///
+/// Throws: std::runtime_error, via fatal_error(), if a precondition
+/// is violated, or if xml-library calls throw an exception derived
+/// from std::exception. Ctor postconditions are assumed to have been
+/// satisfied and are not tested.
+
+xml_lmi::Document const& xml_lmi::dom_parser::document() const
+{
+    try
+        {
+        return parser_->get_document();
+        }
+    catch(std::exception const& e)
+        {
+        fatal_error() << error_context_ << e.what() << LMI_FLUSH;
+        throw std::logic_error("Unreachable"); // Silence compiler warning.
+        }
+}
+#endif // defined USING_CURRENT_XMLWRAPP
+
+/// Return the parsed document's root node.
+///
+/// Preconditions: member parser_ has a document that is not null and
+/// has a root node; the argument, if not empty, matches the name of
+/// that root node.
+///
+/// Throws: std::runtime_error, via fatal_error(), if a precondition
+/// is violated, or if xml-library calls throw an exception derived
+/// from std::exception. Ctor postconditions are assumed to have been
+/// satisfied and are not tested.
+
+xml_lmi::Element const& xml_lmi::dom_parser::root_node
+    (std::string const& expected_name
+    ) const
+{
+    try
+        {
+#if defined USING_CURRENT_XMLWRAPP
+        xml_lmi::Document const& document = parser_->get_document();
+        xml_lmi::Element const& root = document.get_root_node();
+#else  // !defined USING_CURRENT_XMLWRAPP
+        xml_lmi::Element const& root = parser_->get_root_node();
+#endif // !defined USING_CURRENT_XMLWRAPP
+        if(!expected_name.empty() && expected_name != root.get_name())
+            {
+            std::ostringstream oss;
+            oss
+                << "Root-node name is '"
+                << root.get_name()
+                << "', but '"
+                << expected_name
+                << "' was expected."
+                ;
+            throw std::runtime_error(oss.str());
+            }
+
+        return root;
+        }
+    catch(std::exception const& e)
+        {
+        fatal_error() << error_context_ << e.what() << LMI_FLUSH;
+        throw std::logic_error("Unreachable"); // Silence compiler warning.
+        }
+}
+
+// TODO ?? Second argument not yet implemented.
+xml_lmi::ElementContainer child_elements
+    (xml_lmi::Element const& parent
+    ,std::string const& // name
+    )
+{
+    try
+        {
+        xml_lmi::ElementContainer z;
+        typedef Element::const_iterator eci;
+        for(eci i = parent.begin(); i != parent.end(); ++i)
+            {
+            if(!i->is_text())
+                {
+#if defined USING_CURRENT_XMLWRAPP
+// Note that this:
+//   z.push_back(&*i);
+// does not work.
+                z.push_back(i->self());
+#else  // !defined USING_CURRENT_XMLWRAPP
+                z.push_back(ElementPointer(i));
+#endif // !defined USING_CURRENT_XMLWRAPP
+                }
+            }
+        return z;
+        }
+    catch(std::exception const& e)
+        {
+        fatal_error() << e.what() << LMI_FLUSH;
+        throw std::logic_error("Unreachable"); // Silence compiler warning.
+        }
+}
 } // namespace xml_lmi
 
