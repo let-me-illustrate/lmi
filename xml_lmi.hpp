5ef9fb8cc5e0be9a98cceea31256ffc7f964926f
Greg Chicares <gchicares@massmutual.com>
2006-11-04 14:23:07 +0000

    Add xmlwrapp wrapper
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/lmi/lmi/trunk@2191 8995fb05-a277-41b4-a4d2-e84fd280a9a6

---
+++ xml_lmi.hpp
@@ -0,0 +1,43 @@
+// Interface to xmlwrapp.
+//
+// Copyright (C) 2006 Gregory W. Chicares.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+//
+// http://savannah.nongnu.org/projects/lmi
+// email: <chicares@cox.net>
+// snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
+
+// $Id: xml_lmi.hpp,v 1.2 2006-11-04 14:23:07 chicares Exp $
+
+#ifndef xml_lmi_hpp
+#define xml_lmi_hpp
+
+#include "config.hpp"
+
+#include "xml_lmi_fwd.hpp"
+
+#include <xmlwrapp/node.h> // xml::node::const_iterator
+
+#include <vector>
+
+/// Interface to xmlwrapp.
+
+namespace xml_lmi
+{
+    typedef std::vector<xml::node::const_iterator> ElementContainer;
+} // namespace xml_lmi
+
+#endif //xml_lmi_hpp
+
