6cde9013ab9c47c54d3cd1d659fda7ec4179af85
Greg Chicares <gchicares@massmutual.com>
2006-11-10 14:09:38 +0000

    Add calculation-summary elements
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/lmi/lmi/trunk@2232 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- configurable_settings.hpp
+++ configurable_settings.hpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: configurable_settings.hpp,v 1.13 2006-09-20 16:06:24 chicares Exp $
+// $Id: configurable_settings.hpp,v 1.14 2006-11-10 14:09:38 chicares Exp $
 
 #ifndef configurable_settings_hpp
 #define configurable_settings_hpp
@@ -42,7 +42,10 @@
 /// It is implemented as a simple Meyers singleton, with the expected
 /// dead-reference and threading issues.
 ///
-/// Data members, in logical rather than alphabetical order:
+/// Configurable data members:
+///
+/// calculation_summary_columns_: Whitespace-delimited list of columns
+/// to be shown on the calculation summary.
 ///
 /// cgi_bin_log_filename_: Name of log file used for cgicc's debugging
 /// facility.
@@ -69,6 +72,9 @@
 /// typical for the user's preferred spreadsheet program, used to
 /// determine mimetype or msw 'file association'.
 ///
+/// xml_schema_filename_: xsd for ledger xml data and 'format.xml',
+/// used by calculation summary to validate xml resources.
+///
 /// xsl_fo_command_: Command to execute xsl 'formatting objects'
 /// processor. Making this an external command permits using a program
 /// with a free but not GPL-compatible license, such as apache fop,
@@ -76,6 +82,38 @@
 ///
 /// xsl_fo_directory_: Directory where xsl 'formatting objects'
 /// processor resides.
+///
+/// xsl_directory_: Directory where xsl templates, 'schema.xsd'
+/// and 'format.xml' reside.
+///
+/// xslt_format_xml_filename_: xml file containing column titles and
+/// value formats used by calculation summary.
+///
+/// xslt_html_filename_: xsl template that produces calculation-
+/// summary html output from ledger xml data.
+///
+/// TODO ?? CALCULATION_SUMMARY Names should indicate function, not "lightness".
+///
+/// xslt_light_tab_delimited_filename_: xsl template that produces
+/// calculation-summary tab-delimited output suitable for pasting into
+/// a spreadsheet.
+///
+/// xslt_tab_delimited_filename_: xsl template that produces
+/// comprehensive tab-delimited output suitable for pasting into
+/// a spreadsheet.
+///
+/// TODO ?? CALCULATION_SUMMARY Should all recently-added settings be
+/// configurable? Why would a user configure the following, e.g.?
+///   xml_schema_filename_
+///   xslt_format_xml_filename_
+///   xslt_html_filename_
+///   xslt_light_tab_delimited_filename_
+///   xslt_tab_delimited_filename_
+/// Shouldn't these be in 'global_settings.?pp' instead?
+///
+/// TODO ?? CALCULATION_SUMMARY Should this
+///   xsl_directory_
+/// be distinct from what 'data_directory.?pp' already does?
 
 class LMI_SO configurable_settings
     :public MemberSymbolTable<configurable_settings>
@@ -85,24 +123,32 @@ class LMI_SO configurable_settings
   public:
     static configurable_settings& instance();
 
-    std::string const& cgi_bin_log_filename      () const;
-    std::string const& custom_input_filename     () const;
-    std::string const& custom_output_filename    () const;
-    std::string const& default_input_filename    () const;
-    std::string const& libraries_to_preload      () const;
-    bool               offer_hobsons_choice      () const;
-    std::string const& skin_filename             () const;
-    std::string const& spreadsheet_file_extension() const;
-    std::string const& xsl_fo_command            () const;
-    std::string const& xsl_fo_directory          () const;
+    std::string const& calculation_summary_columns      () const;
+    std::string const& cgi_bin_log_filename             () const;
+    std::string const& custom_input_filename            () const;
+    std::string const& custom_output_filename           () const;
+    std::string const& default_input_filename           () const;
+    std::string const& libraries_to_preload             () const;
+    bool               offer_hobsons_choice             () const;
+    std::string const& skin_filename                    () const;
+    std::string const& spreadsheet_file_extension       () const;
+    std::string const& xml_schema_filename              () const;
+    std::string const& xsl_fo_command                   () const;
+    std::string const& xsl_fo_directory                 () const;
+    std::string const& xsl_directory                    () const;
+    std::string const& xslt_format_xml_filename         () const;
+    std::string const& xslt_html_filename               () const;
+    std::string const& xslt_light_tab_delimited_filename() const;
+    std::string const& xslt_tab_delimited_filename      () const;
 
   private:
     configurable_settings();
 
     void ascribe_members();
 
-    std::string const& configuration_filename();
+    std::string const& configuration_filename() const;
 
+    std::string calculation_summary_columns_;
     std::string cgi_bin_log_filename_;
     std::string custom_input_filename_;
     std::string custom_output_filename_;
@@ -111,8 +157,14 @@ class LMI_SO configurable_settings
     bool        offer_hobsons_choice_;
     std::string skin_filename_;
     std::string spreadsheet_file_extension_;
+    std::string xml_schema_filename_;
     std::string xsl_fo_command_;
     std::string xsl_fo_directory_;
+    std::string xsl_directory_;
+    std::string xslt_format_xml_filename_;
+    std::string xslt_html_filename_;
+    std::string xslt_light_tab_delimited_filename_;
+    std::string xslt_tab_delimited_filename_;
 
 #ifdef __BORLANDC__
 // COMPILER !! Borland compilers defectively [11/5] require a public dtor; see:
