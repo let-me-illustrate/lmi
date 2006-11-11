952fb3d3b1cffa9bf04d934443095edd5f488cc5
Gregory W. Chicares <chicares@sbcglobal.net>
2006-11-11 20:13:08 +0000

    Replace 'xsl_fo_directory' with 'print_directory'

--- configurable_settings.cpp
+++ configurable_settings.cpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: configurable_settings.cpp,v 1.22 2006-11-11 02:12:13 chicares Exp $
+// $Id: configurable_settings.cpp,v 1.23 2006-11-11 20:13:08 chicares Exp $
 
 #ifdef __BORLANDC__
 #   include "pchfile.hpp"
@@ -117,11 +117,11 @@ configurable_settings::configurable_settings()
     ,default_input_filename_           ("etc/opt/default.ini")
     ,libraries_to_preload_             (""                   )
     ,offer_hobsons_choice_             (false                )
+    ,print_directory_                  ("/var/opt/lmi/spool" )
     ,skin_filename_                    ("xml_notebook.xrc"   )
     ,spreadsheet_file_extension_       (".gnumeric"          )
     ,xml_schema_filename_              ("schema.xsd"         )
     ,xsl_fo_command_                   ("fo"                 )
-    ,xsl_fo_directory_                 ("/usr/bin/fo"        )
     ,xsl_directory_                    (""                   )
     ,xslt_format_xml_filename_         ("format.xml"         )
     ,xslt_html_filename_               ("html.xsl"           )
@@ -159,11 +159,11 @@ void configurable_settings::ascribe_members()
     ascribe("default_input_filename"           ,&configurable_settings::default_input_filename_           );
     ascribe("libraries_to_preload"             ,&configurable_settings::libraries_to_preload_             );
     ascribe("offer_hobsons_choice"             ,&configurable_settings::offer_hobsons_choice_             );
+    ascribe("print_directory"                  ,&configurable_settings::print_directory_                  );
     ascribe("skin_filename"                    ,&configurable_settings::skin_filename_                    );
     ascribe("spreadsheet_file_extension"       ,&configurable_settings::spreadsheet_file_extension_       );
     ascribe("xml_schema_filename"              ,&configurable_settings::xml_schema_filename_              );
     ascribe("xsl_fo_command"                   ,&configurable_settings::xsl_fo_command_                   );
-    ascribe("xsl_fo_directory"                 ,&configurable_settings::xsl_fo_directory_                 );
     ascribe("xsl_directory"                    ,&configurable_settings::xsl_directory_                    );
     ascribe("xslt_format_xml_filename"         ,&configurable_settings::xslt_format_xml_filename_         );
     ascribe("xslt_html_filename"               ,&configurable_settings::xslt_html_filename_               );
@@ -236,6 +236,11 @@ bool configurable_settings::offer_hobsons_choice() const
     return offer_hobsons_choice_;
 }
 
+std::string const& configurable_settings::print_directory() const
+{
+    return print_directory_;
+}
+
 std::string const& configurable_settings::skin_filename() const
 {
     return skin_filename_;
@@ -254,11 +259,6 @@ std::string const& configurable_settings::xml_schema_filename() const
 std::string const& configurable_settings::xsl_fo_command() const
 {
     return xsl_fo_command_;
-}
-
-std::string const& configurable_settings::xsl_fo_directory() const
-{
-    return xsl_fo_directory_;
 }
 
 std::string const& configurable_settings::xsl_directory() const
