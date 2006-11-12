8ce461379092595f5669a5941a5331a95a51f83f
Greg Chicares <gchicares@massmutual.com>
2006-11-12 17:49:45 +0000

    Import an enumeration from 'gnome-xml-branch'
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/lmi/lmi/trunk@2252 8995fb05-a277-41b4-a4d2-e84fd280a9a6

---
+++ ledger_excerpt.hpp
@@ -0,0 +1,42 @@
+// Ledger excerption for various reports.
+//
+// Copyright (C) 2006 Gregory W. Chicares.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+//
+// http://savannah.nongnu.org/projects/lmi
+// email: <chicares@cox.net>
+// snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
+
+// $Id: ledger_excerpt.hpp,v 1.1 2006-11-12 17:49:45 chicares Exp $
+
+#ifndef ledger_excerpt_hpp
+#define ledger_excerpt_hpp
+
+#include "config.hpp"
+
+/// We need to generate different variants of xml:
+/// - a light version, when speed is crucial (calculation summary)
+/// - a detailed version
+///   TODO ?? EVGENIY--What does e_xml_detailed mean?
+/// - a full version, when we need all the information
+
+enum enum_xml_version
+    {e_xml_calculation_summary
+    ,e_xml_detailed
+    ,e_xml_full
+    };
+
+#endif // ledger_excerpt_hpp
+
