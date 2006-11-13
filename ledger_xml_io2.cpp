2928c8350af2dd676bded94872ef9708e8a2c2d9
Gregory W. Chicares <chicares@sbcglobal.net>
2006-11-13 16:07:57 +0000

    Require policy-year column

--- ledger_xml_io2.cpp
+++ ledger_xml_io2.cpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: ledger_xml_io2.cpp,v 1.1 2006-11-12 19:56:51 chicares Exp $
+// $Id: ledger_xml_io2.cpp,v 1.2 2006-11-13 16:07:57 chicares Exp $
 
 #include "ledger.hpp"
 
@@ -772,6 +772,10 @@ void Ledger::write_excerpt
         supplemental_report_columns.push_back(value_id::from_report_column_title(ledger_invariant_->SupplementalReportColumn10));
         supplemental_report_columns.push_back(value_id::from_report_column_title(ledger_invariant_->SupplementalReportColumn11));
         }
+
+    std::vector<value_id> required_columns;
+    required_columns.push_back(value_id::from_name("PolicyYear"));
+    formatter.add_columns_to_format(required_columns);
 
     std::vector<value_id> calculation_summary_columns;
     try
