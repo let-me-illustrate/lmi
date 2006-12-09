ef6fc2f1747035ecba97124b97aa90af352c4a42
Greg Chicares <gchicares@massmutual.com>
2006-12-09 21:24:16 +0000

    Don't attempt to dereference an end-of-stream istreambuf_iterator
    git-svn-id: svn://svn.sv.gnu.org/lmi/lmi/trunk@2366 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- input_test.cpp
+++ input_test.cpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: input_test.cpp,v 1.16 2006-12-06 16:23:17 chicares Exp $
+// $Id: input_test.cpp,v 1.17 2006-12-09 21:24:16 chicares Exp $
 
 #ifdef __BORLANDC__
 #   include "pchfile.hpp"
@@ -66,7 +66,7 @@ bool streams_are_identical(std::istream& is0, std::istream& is1)
             return false;
             }
         }
-    return *i == *j;
+    return end == i && end == j;
 }
 
 bool files_are_identical(std::string const& file0, std::string const& file1)
