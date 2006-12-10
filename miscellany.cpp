c6f04402b7f59643622486d19d911049b5ee818b
Gregory W. Chicares <chicares@sbcglobal.net>
2006-12-10 16:19:22 +0000

    Refactor

--- miscellany.cpp
+++ miscellany.cpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: miscellany.cpp,v 1.3 2006-01-29 13:52:00 chicares Exp $
+// $Id: miscellany.cpp,v 1.4 2006-12-10 16:19:22 chicares Exp $
 
 #ifdef __BORLANDC__
 #   include "pchfile.hpp"
@@ -29,7 +29,42 @@
 #include "miscellany.hpp"
 
 #include <ctime>
+#include <fstream>
+#include <istream>
+#include <iterator>
 #include <stdexcept>
+
+namespace
+{
+/// This function is a derived work adapted from usenet article
+/// <1eo2sct.ggkc9z84ko0eN%cbarron3@ix.netcom.com>. GWC rewrote it in
+/// 2005. Any defect should not reflect on Carl Barron's reputation.
+
+bool streams_are_identical(std::istream& is0, std::istream& is1)
+{
+    std::istreambuf_iterator<char> i(is0);
+    std::istreambuf_iterator<char> j(is1);
+    std::istreambuf_iterator<char> end;
+
+    for(; end != i && end != j; ++i, ++j)
+        {
+        if(*i != *j)
+            {
+            return false;
+            }
+        }
+    return end == i && end == j;
+}
+} // Unnamed namespace.
+
+// TODO ?? Add unit tests.
+
+bool files_are_identical(std::string const& file0, std::string const& file1)
+{
+    std::ifstream is0(file0.c_str());
+    std::ifstream is1(file1.c_str());
+    return streams_are_identical(is0, is1);
+}
 
 // http://groups-beta.google.com/group/borland.public.cpp.borlandcpp/msg/638d1f25e66472d9
 // 18 Jul 2001 18:25:15 -0400
