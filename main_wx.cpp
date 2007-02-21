545addd648b65a6f49a820bd07bf4e6256e3eb70
Gregory W. Chicares <chicares@sbcglobal.net>
2007-02-21 11:19:13 +0000

    Make wxDocTemplate accelerators unique

--- main_wx.cpp
+++ main_wx.cpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: main_wx.cpp,v 1.68 2007-02-21 11:15:21 chicares Exp $
+// $Id: main_wx.cpp,v 1.69 2007-02-21 11:19:13 chicares Exp $
 
 // Portions of this file are derived from wxWindows files
 //   samples/docvwmdi/docview.cpp (C) 1998 Julian Smart and Markus Holzem
@@ -371,7 +371,7 @@ void Skeleton::InitDocManager()
 
     new(wx) wxDocTemplate
         (doc_manager_
-        ,"Text"
+        ,"Te&xt"
         ,"*.txt"
         ,""
         ,"txt"
