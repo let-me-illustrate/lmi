0199c0c045984d6ea882cf3a596d0bc7a477b5f3
Greg Chicares <gchicares@massmutual.com>
2007-05-12 19:05:33 +0000

    Implement display mode variations; improve unit test
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/lmi/lmi/trunk@2795 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- progress_meter_test.cpp
+++ progress_meter_test.cpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: progress_meter_test.cpp,v 1.6 2007-05-11 01:28:16 chicares Exp $
+// $Id: progress_meter_test.cpp,v 1.7 2007-05-12 19:05:33 chicares Exp $
 
 #ifdef __BORLANDC__
 #   include "pchfile.hpp"
@@ -30,28 +30,95 @@
 
 #include "test_tools.hpp"
 
+#include <sstream>
+
 struct progress_meter_test
 {
-    static void test();
+    static void test_normal_usage();
+    static void test_quiet_display_mode();
+    static void test_empty_title_and_zero_max_count();
+    static void test_invalid_display_mode();
 };
 
-void progress_meter_test::test()
+void progress_meter_test::test_normal_usage()
 {
+    progress_meter_unit_test_stream().str("");
     int const max_count = 3;
-    boost::shared_ptr<progress_meter> meter(create_progress_meter(max_count));
+    boost::shared_ptr<progress_meter> meter
+        (create_progress_meter
+            (max_count
+            ,"Some title"
+            ,progress_meter::e_unit_test_mode
+            )
+        );
     for(int i = 0; i < max_count; ++i)
         {
         // Intended use: do some work, then update meter at end of block.
         BOOST_TEST(meter->reflect_progress());
         // Of course, you could do more work here, too, if you like.
         }
-
+    BOOST_TEST_EQUAL("Some title...", progress_meter_unit_test_stream().str());
     BOOST_TEST_THROW(meter->reflect_progress(), std::logic_error, "");
+}
+
+void progress_meter_test::test_quiet_display_mode()
+{
+    int const max_count = 3;
+    boost::shared_ptr<progress_meter> meter
+        (create_progress_meter
+            (max_count
+            ,"ERROR: THIS SHOULD NOT DISPLAY"
+            ,progress_meter::e_quiet_display
+            )
+        );
+    for(int i = 0; i < max_count; ++i)
+        {
+        BOOST_TEST(meter->reflect_progress());
+        }
+    BOOST_TEST_THROW(meter->reflect_progress(), std::logic_error, "");
+}
+
+void progress_meter_test::test_empty_title_and_zero_max_count()
+{
+    progress_meter_unit_test_stream().str("");
+    int const max_count = 0;
+    boost::shared_ptr<progress_meter> meter
+        (create_progress_meter
+            (max_count
+            ,""
+            ,progress_meter::e_unit_test_mode
+            )
+        );
+    for(int i = 0; i < max_count; ++i)
+        {
+        BOOST_TEST(meter->reflect_progress());
+        }
+    BOOST_TEST(progress_meter_unit_test_stream().str().empty());
+    BOOST_TEST_THROW(meter->reflect_progress(), std::logic_error, "");
+}
+
+void progress_meter_test::test_invalid_display_mode()
+{
+    progress_meter_unit_test_stream().str("");
+    int const max_count = 0;
+    BOOST_TEST_THROW
+        (boost::shared_ptr<progress_meter> meter
+            (create_progress_meter
+                (max_count
+                ,""
+                ,progress_meter::enum_display_mode(99)
+                )
+            )
+        ,std::runtime_error
+        ,"Case 99 not found."
+        );
 }
 
 int test_main(int, char*[])
 {
-    progress_meter_test::test();
+    progress_meter_test::test_normal_usage();
+    progress_meter_test::test_empty_title_and_zero_max_count();
+    progress_meter_test::test_invalid_display_mode();
     return EXIT_SUCCESS;
 }
 
