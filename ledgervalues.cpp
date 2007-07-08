2b2d1f00d92ef9bb54324f5a5d6dddb9c53c79f6
Greg Chicares <gchicares@massmutual.com>
2007-07-08 02:22:35 +0000

    Copy a shared pointer, not its contents
    git-svn-id: svn://svn.sv.gnu.org/lmi/lmi/trunk@3190 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- ledgervalues.cpp
+++ ledgervalues.cpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: ledgervalues.cpp,v 1.29 2007-07-08 01:42:23 chicares Exp $
+// $Id: ledgervalues.cpp,v 1.30 2007-07-08 02:22:35 chicares Exp $
 
 #ifdef __BORLANDC__
 #   include "pchfile.hpp"
@@ -58,10 +58,8 @@ double IllusVal::Run(InputParms const* IP)
     av.SetDebugFilename(debug_filename.string());
 
     double z = av.RunAV();
-    // TODO ?? This seems nasty, but probably this whole class should
-    // be eradicated.
-    LMI_ASSERT(av.ledger_from_av().get());
-    ledger_ = boost::shared_ptr<Ledger>(new Ledger(*av.ledger_from_av()));
+    ledger_ = av.ledger_from_av();
+    LMI_ASSERT(ledger_.get());
 
     return z;
 }
