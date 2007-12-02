90f322bfb2360411ef3f6ac74ed0a8dbbdf6a0c7
Greg Chicares <gchicares@massmutual.com>
2007-12-02 01:18:10 +0000

    Prune empty cvs directories
    git-svn-id: svn://svn.sv.gnu.org/lmi/lmi/trunk@3391 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- gwc/install_msw.sh
+++ gwc/install_msw.sh
@@ -21,7 +21,7 @@
 # email: <chicares@cox.net>
 # snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-# $Id: install_msw.sh,v 1.5 2007-11-28 17:40:30 chicares Exp $
+# $Id: install_msw.sh,v 1.6 2007-12-02 01:18:10 chicares Exp $
 
 set -v
 
@@ -55,7 +55,7 @@ cd /opt/lmi/src
 
 export CVS_RSH="ssh"
 export CVSROOT=":pserver:anonymous@cvs.savannah.nongnu.org:/sources/lmi"
-cvs -z3 checkout lmi
+cvs -z3 checkout -P lmi
 
 cd /opt/lmi/src/lmi
 
