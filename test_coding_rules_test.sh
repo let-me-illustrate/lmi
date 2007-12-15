c3689f7eea38c430d3d974aa4c062846ea238d43
Greg Chicares <gchicares@massmutual.com>
2007-12-15 17:56:23 +0000

    Add an automated test
    git-svn-id: svn://svn.sv.gnu.org/lmi/lmi/trunk@3421 8995fb05-a277-41b4-a4d2-e84fd280a9a6

---
+++ test_coding_rules_test.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# Test files for consistency with various rules: unit test.
+
+# Copyright (C) 2007 Gregory W. Chicares.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+#
+# http://savannah.nongnu.org/projects/lmi
+# email: <chicares@cox.net>
+# snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
+
+# $Id: test_coding_rules_test.sh,v 1.1 2007-12-15 17:56:23 chicares Exp $
+
+echo "Testing 'test_coding_rules'."
+
+rm --force eraseme*
+
+cat >eraseme0 <<EOF
+Copyright
+`date -u +'%Y'`
+EOF
+
+cat >eraseme1 <<EOF
+Copyright `date -u +'%Y'`
+EOF
+touch --date=19700101 eraseme1
+
+./test_coding_rules a_nonexistent_file eraseme* >eraseme_observed 2>&1
+
+cat >eraseme_expected <<'EOF'
+File 'a_nonexistent_file' not found.
+File 'eraseme0' lacks current copyright.
+File 'eraseme1' lacks current copyright.
+EOF
+
+diff -u0 eraseme_observed eraseme_expected && rm --force eraseme*
+
