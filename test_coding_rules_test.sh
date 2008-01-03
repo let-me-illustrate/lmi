fa66e9cd58afa945330320bef30a79f01cc022d8
Gregory W. Chicares <chicares@sbcglobal.net>
2008-01-03 04:46:23 +0000

    Rename test files to increase clarity

--- test_coding_rules_test.sh
+++ test_coding_rules_test.sh
@@ -21,7 +21,7 @@
 # email: <chicares@cox.net>
 # snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-# $Id: test_coding_rules_test.sh,v 1.23 2008-01-03 03:16:13 chicares Exp $
+# $Id: test_coding_rules_test.sh,v 1.24 2008-01-03 04:46:23 chicares Exp $
 
 echo "Testing 'test_coding_rules'."
 
@@ -43,18 +43,18 @@ boilerplate="$good_copyright $good_rcsid $good_url"
 
 # Files in general.
 
-cat >eraseme000 <<EOF
+cat >eraseme_000 <<EOF
 $boilerplate
 EOF
 
 # Files in general: copyright.
 
-cat >eraseme_copyright000 <<EOF
+cat >eraseme_copyright_000 <<EOF
 $boilerplate
 "(C) 1999": upper-case 'C' is correct in ASCII copyright symbol.
 EOF
 
-cat >eraseme_copyright001 <<EOF
+cat >eraseme_copyright_001 <<EOF
 $good_rcsid $good_url
 Copyright (C)
   is expected to be on the same line as
@@ -62,27 +62,27 @@ Copyright (C)
 "(c) 1999": lower-case 'c' is incorrect in ASCII copyright symbol.
 EOF
 
-cat >eraseme_copyright002 <<EOF
+cat >eraseme_copyright_002 <<EOF
 $good_rcsid $good_url
 This would seem to be all right:
 // Copyright (C) 1900, `date -u +'%Y'`, 2100
 but the datestamp is changed to the beginning of the msw epoch.
 Don't use the unix epoch, because that causes mayhem on msw.
 EOF
-touch --date=19800102 eraseme_copyright002
+touch --date=19800102 eraseme_copyright_002
 
 # Files in general: defect markers.
 
 Q='?'
 
-cat >eraseme_marker000 <<EOF
+cat >eraseme_marker_000 <<EOF
 $boilerplate
  TODO ${Q}${Q} Okay.
  INELEGANT !! Okay.
  number_of_valid_pointers += !!p; // This legitimate usage is allowed.
 EOF
 
-cat >eraseme_marker001 <<EOF
+cat >eraseme_marker_001 <<EOF
 $boilerplate
  TODO${Q}${Q} Bad spacing.
  TODO ${Q}${Q}Bad spacing.
@@ -97,44 +97,44 @@ EOF
 
 # Files in general: RCS Ids.
 
-cat >eraseme_rcs_id000 <<EOF
+cat >eraseme_rcs_id_000 <<EOF
 $good_copyright $good_url
 A real RCS Id must be accepted:
 \$\
 Id: test_coding_rules_test.sh,v 1.13 2007/12/29 14:16:18 chicares Exp \$
 EOF
 
-cat >eraseme_rcs_id001 <<EOF
+cat >eraseme_rcs_id_001 <<EOF
 $good_copyright $good_url
 \$\
 Id\$ # Okay.
 EOF
 
-cat >eraseme_rcs_id002 <<EOF
+cat >eraseme_rcs_id_002 <<EOF
 $good_copyright $good_url
 \$\
 Id # Lacks terminal dollar sign.
 EOF
 
-cat >eraseme_rcs_id003 <<EOF
+cat >eraseme_rcs_id_003 <<EOF
 $good_copyright $good_url
 \$\
 Id
 \$ # Terminal dollar sign must be on same line.
 EOF
 
-cat >eraseme_rcs_id004 <<EOF
+cat >eraseme_rcs_id_004 <<EOF
 $good_copyright $good_url
 # No RCS Id at all.
 EOF
 
-cat >eraseme_rcs_id005 <<EOF
+cat >eraseme_rcs_id_005 <<EOF
 $good_copyright $good_url
 \$\
 Id gibberish\$ # Lacks colon.
 EOF
 
-cat >eraseme_rcs_id006 <<EOF
+cat >eraseme_rcs_id_006 <<EOF
 $good_copyright $good_url
 // \$\
 Id: README.auto,v 1.6 2007/02/23 12:43:25 chicares Exp \$
@@ -144,11 +144,11 @@ EOF
 
 # Files in general: taboos.
 
-cat >eraseme_taboo000 <<EOF
+cat >eraseme_taboo_000 <<EOF
 $boilerplate
 EOF
 
-cat >eraseme_taboo001 <<EOF
+cat >eraseme_taboo_001 <<EOF
 $boilerplate
 675 Mass Ave, Cambridge, MA
 59 Temple Place, Suite 330
@@ -157,59 +157,59 @@ EOF
 
 # Files in general: lmi url.
 
-cat >eraseme_url000 <<EOF
+cat >eraseme_url_000 <<EOF
 $good_copyright $good_rcsid
 <http://savannah.nongnu.org/projects/lmi>
 EOF
 
-cat >eraseme_url001 <<EOF
+cat >eraseme_url_001 <<EOF
 $good_copyright $good_rcsid
 EOF
 
 # Files in general: whitespace.
 
-cat >eraseme_whitespace000 <<EOF
+cat >eraseme_whitespace_000 <<EOF
 $boilerplate
 Spaces are permitted; they  can   be    consecutive.
 EOF
 
 ascii_ff=$'\f'
-cat >eraseme_whitespace001 <<EOF
+cat >eraseme_whitespace_001 <<EOF
 $boilerplate
 $ascii_ff
 EOF
 
 ascii_cr=$'\r'
-cat >eraseme_whitespace002 <<EOF
+cat >eraseme_whitespace_002 <<EOF
 $boilerplate
 $ascii_cr
 EOF
 
 ascii_ht=$'\t'
-cat >eraseme_whitespace003 <<EOF
+cat >eraseme_whitespace_003 <<EOF
 $boilerplate
 $ascii_ht
 EOF
 
 ascii_vt=$'\v'
-cat >eraseme_whitespace004 <<EOF
+cat >eraseme_whitespace_004 <<EOF
 $boilerplate
 $ascii_vt
 EOF
 
 # C++ source files.
 
-cat >eraseme000.cpp <<EOF
+cat >eraseme_cpp_000.cpp <<EOF
 $boilerplate
 EOF
 
-cat >eraseme001.cpp <<EOF
+cat >eraseme_cpp_001.cpp <<EOF
 $boilerplate
 This header must be included only by other headers:
 #include "config.hpp"
 EOF
 
-cat >eraseme002.cpp <<EOF
+cat >eraseme_cpp_002.cpp <<EOF
 $boilerplate
 Labels must be indented either two spaces:
   l0:
@@ -229,55 +229,55 @@ EOF
 
 # Headers.
 
-cat >eraseme000.hpp <<EOF
+cat >eraseme_hpp_000.hpp <<EOF
 $boilerplate
-#ifndef eraseme000_hpp
-#define eraseme000_hpp
+#ifndef eraseme_hpp_000_hpp
+#define eraseme_hpp_000_hpp
 #include "config.hpp"
-#endif // eraseme000_hpp
+#endif // eraseme_hpp_000_hpp
 EOF
 
-cat >eraseme001.hpp <<EOF
+cat >eraseme_hpp_001.hpp <<EOF
 $boilerplate
-#ifndef eraseme001_hpp
-#define eraseme001_hpp
+#ifndef eraseme_hpp_001_hpp
+#define eraseme_hpp_001_hpp
 #include "config.hpp"
 Name in comment is required to match:
-#endif // eraseme000_hpp
+#endif // eraseme_hpp_000_hpp
 EOF
 
-cat >eraseme002.hpp <<EOF
+cat >eraseme_hpp_002.hpp <<EOF
 $boilerplate
 Stray comments on include guards are forbidden as meaningless.
-#ifndef eraseme002_hpp // Here's a guard to make the header idempotent.
-#define eraseme002_hpp // But such a comment is only distracting.
+#ifndef eraseme_hpp_002_hpp // Here's a guard to make the header idempotent.
+#define eraseme_hpp_002_hpp // But such a comment is only distracting.
 #include "config.hpp"
-#endif // eraseme002_hpp
+#endif // eraseme_hpp_002_hpp
 EOF
 
-cat >eraseme003.hpp <<EOF
+cat >eraseme_hpp_003.hpp <<EOF
 $boilerplate
-#ifndef eraseme003_hpp
-#define eraseme003_hpp
+#ifndef eraseme_hpp_003_hpp
+#define eraseme_hpp_003_hpp
 This compulsory include directive must occur, in canonical form (with
 only one space, preceding '"'), before any other include directive.
 #   include "config.hpp"
 #include "config.hpp" // Stray comments forbidden.
 #include "config.hpp"
-#endif // eraseme003_hpp
+#endif // eraseme_hpp_003_hpp
 EOF
 
-cat >eraseme004.hpp <<EOF
+cat >eraseme_hpp_004.hpp <<EOF
 $boilerplate
-#ifndef eraseme004_hpp
-#define eraseme004_hpp
+#ifndef eraseme_hpp_004_hpp
+#define eraseme_hpp_004_hpp
 Missing compulsory include directive.
-#endif // eraseme004_hpp
+#endif // eraseme_hpp_004_hpp
 EOF
 
 # Makefiles.
 
-cat >eraseme000.make <<EOF
+cat >eraseme_make_000.make <<EOF
 ${ascii_ht}
 $boilerplate
 A line's initial tab can be followed by any other character.
@@ -285,13 +285,13 @@ ${ascii_ht}This is okay.
 ${ascii_ht}
 EOF
 
-cat >eraseme001.make <<EOF
+cat >eraseme_make_001.make <<EOF
 $boilerplate
 Consecutive tabs are forbidden.
 ${ascii_ht}${ascii_ht}
 EOF
 
-cat >eraseme002.make <<EOF
+cat >eraseme_make_002.make <<EOF
 $boilerplate
 Tab can occur only at the beginning of a line.
  ${ascii_ht}
@@ -300,12 +300,12 @@ EOF
 # X pixmaps require no copyright, but do require 'const'.
 # SOMEDAY !! Require internal name to match file name?
 
-cat >eraseme000.xpm <<EOF
-static char const* eraseme000_xpm[]
+cat >eraseme_xpm_000.xpm <<EOF
+static char const* eraseme_xpm_000_xpm[]
 EOF
 
-cat >eraseme001.xpm <<EOF
-static char* eraseme001_xpm[]
+cat >eraseme_xpm_001.xpm <<EOF
+static char* eraseme_xpm_001_xpm[]
 EOF
 
 # Compare observed to expected.
@@ -315,47 +315,47 @@ EOF
 cat >eraseme_expected <<EOF
 Exception--file '.': Argument is a directory.
 Exception--file 'a_nonexistent_file': File not found.
-File 'eraseme001.cpp' must not include 'config.hpp'.
-File 'eraseme001.hpp' has noncanonical header guards.
-Exception--file 'eraseme001.make': File contains postinitial '\t'.
-File 'eraseme001.xpm' lacks /^static char const\*/.
-File 'eraseme002.cpp' has misindented label ' wrong   :'.
-File 'eraseme002.cpp' has misindented label '   No2   :'.
-File 'eraseme002.cpp' has misindented label '       x_:'.
-File 'eraseme002.hpp' has noncanonical header guards.
-Exception--file 'eraseme002.make': File contains postinitial '\t'.
-File 'eraseme003.hpp' must include 'config.hpp' first.
-File 'eraseme004.hpp' must include 'config.hpp'.
-File 'eraseme004.hpp' lacks line '#include "config.hpp"'.
-File 'eraseme004.hpp' must include 'config.hpp' first.
-File 'eraseme_copyright001' lacks current copyright.
-File 'eraseme_copyright001' breaks taboo '\(c\) *[0-9]'.
-File 'eraseme_copyright002' lacks current copyright.
-File 'eraseme_marker001' has irregular defect marker 'TODO${Q}${Q} '.
-File 'eraseme_marker001' has irregular defect marker 'TODO ${Q}${Q}B'.
-File 'eraseme_marker001' has irregular defect marker 'TODO ${Q}${Q}${Q} '.
-File 'eraseme_marker001' has irregular defect marker 'tODO ${Q}${Q} '.
-File 'eraseme_marker001' has irregular defect marker 'ODO ${Q}${Q} '.
-File 'eraseme_marker001' has irregular defect marker 'INELEGANT!! '.
-File 'eraseme_marker001' has irregular defect marker 'INELEGANT !!B'.
-File 'eraseme_marker001' has irregular defect marker 'INELEGANT !!!'.
-File 'eraseme_marker001' has irregular defect marker 'ELEGANT !! '.
-File 'eraseme_rcs_id002' lacks a well-formed RCS Id.
-File 'eraseme_rcs_id002' contains a malformed RCS Id.
-File 'eraseme_rcs_id003' lacks a well-formed RCS Id.
-File 'eraseme_rcs_id003' contains a malformed RCS Id.
-File 'eraseme_rcs_id004' lacks a well-formed RCS Id.
-File 'eraseme_rcs_id005' lacks a well-formed RCS Id.
-File 'eraseme_rcs_id006' contains more than one RCS Id.
-File 'eraseme_taboo001' breaks taboo 'Cambridge'.
-File 'eraseme_taboo001' breaks taboo 'Temple'.
-File 'eraseme_taboo001' breaks taboo 'Shibboleth'.
-File 'eraseme_taboo001' breaks taboo 'sibboleth'.
-File 'eraseme_url001' lacks lmi URL.
-Exception--file 'eraseme_whitespace001': File contains '\f'.
-Exception--file 'eraseme_whitespace002': File contains '\r' or '\v'.
-Exception--file 'eraseme_whitespace003': File contains '\t'.
-Exception--file 'eraseme_whitespace004': File contains '\r' or '\v'.
+File 'eraseme_copyright_001' lacks current copyright.
+File 'eraseme_copyright_001' breaks taboo '\(c\) *[0-9]'.
+File 'eraseme_copyright_002' lacks current copyright.
+File 'eraseme_cpp_001.cpp' must not include 'config.hpp'.
+File 'eraseme_cpp_002.cpp' has misindented label ' wrong   :'.
+File 'eraseme_cpp_002.cpp' has misindented label '   No2   :'.
+File 'eraseme_cpp_002.cpp' has misindented label '       x_:'.
+File 'eraseme_hpp_001.hpp' has noncanonical header guards.
+File 'eraseme_hpp_002.hpp' has noncanonical header guards.
+File 'eraseme_hpp_003.hpp' must include 'config.hpp' first.
+File 'eraseme_hpp_004.hpp' must include 'config.hpp'.
+File 'eraseme_hpp_004.hpp' lacks line '#include "config.hpp"'.
+File 'eraseme_hpp_004.hpp' must include 'config.hpp' first.
+Exception--file 'eraseme_make_001.make': File contains postinitial '\t'.
+Exception--file 'eraseme_make_002.make': File contains postinitial '\t'.
+File 'eraseme_marker_001' has irregular defect marker 'TODO${Q}${Q} '.
+File 'eraseme_marker_001' has irregular defect marker 'TODO ${Q}${Q}B'.
+File 'eraseme_marker_001' has irregular defect marker 'TODO ${Q}${Q}${Q} '.
+File 'eraseme_marker_001' has irregular defect marker 'tODO ${Q}${Q} '.
+File 'eraseme_marker_001' has irregular defect marker 'ODO ${Q}${Q} '.
+File 'eraseme_marker_001' has irregular defect marker 'INELEGANT!! '.
+File 'eraseme_marker_001' has irregular defect marker 'INELEGANT !!B'.
+File 'eraseme_marker_001' has irregular defect marker 'INELEGANT !!!'.
+File 'eraseme_marker_001' has irregular defect marker 'ELEGANT !! '.
+File 'eraseme_rcs_id_002' lacks a well-formed RCS Id.
+File 'eraseme_rcs_id_002' contains a malformed RCS Id.
+File 'eraseme_rcs_id_003' lacks a well-formed RCS Id.
+File 'eraseme_rcs_id_003' contains a malformed RCS Id.
+File 'eraseme_rcs_id_004' lacks a well-formed RCS Id.
+File 'eraseme_rcs_id_005' lacks a well-formed RCS Id.
+File 'eraseme_rcs_id_006' contains more than one RCS Id.
+File 'eraseme_taboo_001' breaks taboo 'Cambridge'.
+File 'eraseme_taboo_001' breaks taboo 'Temple'.
+File 'eraseme_taboo_001' breaks taboo 'Shibboleth'.
+File 'eraseme_taboo_001' breaks taboo 'sibboleth'.
+File 'eraseme_url_001' lacks lmi URL.
+Exception--file 'eraseme_whitespace_001': File contains '\f'.
+Exception--file 'eraseme_whitespace_002': File contains '\r' or '\v'.
+Exception--file 'eraseme_whitespace_003': File contains '\t'.
+Exception--file 'eraseme_whitespace_004': File contains '\r' or '\v'.
+File 'eraseme_xpm_001.xpm' lacks /^static char const\*/.
 EOF
 
 diff --unified=0 eraseme_expected eraseme_observed && rm --force eraseme*
