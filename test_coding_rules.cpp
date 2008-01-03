05a4eb56cdc9d608249065ec4c393eca7125d425
Greg Chicares <gchicares@massmutual.com>
2008-01-03 02:04:19 +0000

    Explicitly flush stdout to ensure synchronization with stderr
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/lmi/lmi/trunk@3473 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- test_coding_rules.cpp
+++ test_coding_rules.cpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: test_coding_rules.cpp,v 1.37 2008-01-02 13:28:17 chicares Exp $
+// $Id: test_coding_rules.cpp,v 1.38 2008-01-03 02:04:19 chicares Exp $
 
 #ifdef __BORLANDC__
 #   include "pchfile.hpp"
@@ -138,7 +138,7 @@ file::file(std::string const& file_path)
 
 void complain(file const& f, std::string const& complaint)
 {
-    std::cout << "File '" << f.full_name() << "' " << complaint << '\n';
+    std::cout << "File '" << f.full_name() << "' " << complaint << std::endl;
 }
 
 void require
