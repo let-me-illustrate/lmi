00cc2ff9574b8deb1f54bd5c5121548fc4240207
Greg Chicares <gchicares@massmutual.com>
2008-01-21 19:00:59 +0000

    Rename certain experience-rating inputs
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/lmi/lmi/trunk@3539 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- inputillus.cpp
+++ inputillus.cpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: inputillus.cpp,v 1.29 2008-01-21 17:52:23 chicares Exp $
+// $Id: inputillus.cpp,v 1.30 2008-01-21 19:00:58 chicares Exp $
 
 #ifdef __BORLANDC__
 #   include "pchfile.hpp"
@@ -708,8 +708,8 @@ void IllusInputParms::ascribe_members()
     ascribe("OverrideExperienceReserveRate", &IllusInputParms::OverrideExperienceReserveRate);
     ascribe("ExperienceReserveRate"        , &IllusInputParms::ExperienceReserveRate      );
     ascribe("ExperienceRatingInitialKFactor",&IllusInputParms::ExperienceRatingInitialKFactor);
-    ascribe("InforceExperienceReserve"     , &IllusInputParms::InforceExperienceReserve   );
-    ascribe("NetMortalityChargeHistory"    , &IllusInputParms::NetMortalityChargeHistory  );
+    ascribe("InforceNetExperienceReserve"  , &IllusInputParms::InforceNetExperienceReserve);
+    ascribe("InforceYtdNetCoiCharge"       , &IllusInputParms::InforceYtdNetCoiCharge     );
     ascribe("PayLoanInterestInCash"        , &IllusInputParms::PayLoanInt                 );
     ascribe("WithdrawToBasisThenLoan"      , &IllusInputParms::WDToBasisThenLoan          );
     ascribe("UseAverageOfAllFunds"         , &IllusInputParms::AvgFund                    );
