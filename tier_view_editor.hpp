9d38edeece69defe66362f315fa7c0a6a3fe83c3
Greg Chicares <gchicares@massmutual.com>
2008-02-17 16:02:25 +0000

    Include a header that's not obviously unnecessary
    git-svn-id: svn://svn.sv.gnu.org/lmi/lmi/trunk@3584 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- tier_view_editor.hpp
+++ tier_view_editor.hpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: tier_view_editor.hpp,v 1.15 2008-02-17 15:17:15 chicares Exp $
+// $Id: tier_view_editor.hpp,v 1.16 2008-02-17 16:02:25 chicares Exp $
 
 #ifndef tier_view_editor_hpp
 #define tier_view_editor_hpp
@@ -38,6 +38,7 @@
 
 #include <string>
 #include <utility> // std::pair
+#include <vector>
 
 /// Notes on TierEditorGrid and TierTableAdapter.
 ///
