b72844ce2f5dfd6d91684415bca0fa067e8a7628
Greg Chicares <gchicares@massmutual.com>
2008-02-28 17:31:30 +0000

    Set help provider--for now, just wxSimpleHelpProvider
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/lmi/lmi/trunk@3603 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- main_wx.cpp
+++ main_wx.cpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: main_wx.cpp,v 1.99 2008-02-26 02:59:25 chicares Exp $
+// $Id: main_wx.cpp,v 1.100 2008-02-28 17:31:30 chicares Exp $
 
 // Portions of this file are derived from wxWindows files
 //   samples/docvwmdi/docview.cpp (C) 1998 Julian Smart and Markus Holzem
@@ -83,6 +83,7 @@
 #include "wx_utility.hpp"           // class ClipboardEx
 
 #include <wx/config.h>
+#include <wx/cshelp.h>
 #include <wx/docmdi.h>
 #include <wx/image.h>
 #include <wx/log.h>                 // wxSafeShowMessage()
@@ -557,6 +558,8 @@ bool Skeleton::OnInit()
             }
 
         authenticate_system();
+
+        wxHelpProvider::Set(new(wx) wxSimpleHelpProvider);
 
         wxXmlResource& xml_resources = *wxXmlResource::Get();
 
