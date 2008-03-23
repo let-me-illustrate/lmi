c39cad304ce00bab4970567bf22168b9533d2ae1
Greg Chicares <gchicares@massmutual.com>
2008-03-23 00:53:32 +0000

    Improve documentation
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/lmi/lmi/trunk@3625 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- main_wx.cpp
+++ main_wx.cpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: main_wx.cpp,v 1.104 2008-03-21 12:13:17 chicares Exp $
+// $Id: main_wx.cpp,v 1.105 2008-03-23 00:53:32 chicares Exp $
 
 // Portions of this file are derived from wxWindows files
 //   samples/docvwmdi/docview.cpp (C) 1998 Julian Smart and Markus Holzem
@@ -955,8 +955,6 @@ namespace
 ///
 /// Design rationale--see:
 ///   http://lists.nongnu.org/archive/html/lmi/2008-02/msg00005.html
-///
-/// SOMEDAY !! Seek a gtk equivalent for msw-specific code.
 
 void send_paste_message_to(wxWindow const& w)
 {
