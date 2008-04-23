c8c5b7ad15a45b9406a7ef088885f15c975cacb6
Gregory W. Chicares <chicares@sbcglobal.net>
2008-04-23 13:31:32 +0000

    Suppress html help until it's been signed off by testers

--- main_wx.cpp
+++ main_wx.cpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: main_wx.cpp,v 1.106 2008-04-01 10:57:43 chicares Exp $
+// $Id: main_wx.cpp,v 1.107 2008-04-23 13:31:32 chicares Exp $
 
 // Portions of this file are derived from wxWindows files
 //   samples/docvwmdi/docview.cpp (C) 1998 Julian Smart and Markus Holzem
@@ -462,7 +462,9 @@ void Skeleton::InitHelp()
     help_controller_ = new(wx) wxHtmlHelpController(wxHF_DEFAULT_STYLE, frame_);
     LMI_ASSERT(help_controller_);
 
+#if defined DO_NOT_SUPPRESS_HELP
     help_controller_->AddBook(wxFileName(AddDataDir("user_manual.hhp")));
+#endif // defined DO_NOT_SUPPRESS_HELP
 }
 
 void Skeleton::InitIcon()
@@ -534,7 +536,9 @@ void Skeleton::UponEditDefaultCell(wxCommandEvent&)
 
 void Skeleton::UponHelp(wxCommandEvent&)
 {
+#if defined DO_NOT_SUPPRESS_HELP
     help_controller_->DisplayContents();
+#endif // defined DO_NOT_SUPPRESS_HELP
 }
 
 /// Rethrow an exception caught by wx into a local catch clause.
