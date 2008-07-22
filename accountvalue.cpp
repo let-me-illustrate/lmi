e86a0847e445655039ba535ba7ffb480caeed0b9
Greg Chicares <gchicares@massmutual.com>
2008-07-22 17:59:26 +0000

    Refactor, using yare instead of obsolete input for solves
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/lmi/lmi/trunk@3943 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- accountvalue.cpp
+++ accountvalue.cpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: accountvalue.cpp,v 1.55 2008-07-20 18:37:42 chicares Exp $
+// $Id: accountvalue.cpp,v 1.56 2008-07-22 17:59:25 chicares Exp $
 
 #ifdef __BORLANDC__
 #   include "pchfile.hpp"
@@ -604,9 +604,9 @@ void AccountValue::PerformPmtStrategy(double* a_Pmt)
     if
         (
             mce_solve_ee_prem == yare_input_.SolveType
-        &&  Input_->SolveBegYear.value() <= Year
+        &&  yare_input_.SolveBeginYear <= Year
         &&  Year < std::min
-                (Input_->SolveEndYear.value()
+                (yare_input_.SolveEndYear
                 ,BasicValues::GetLength()
                 )
         )
