d63fa48ec141167ad0303605458e51a484123d52
Greg Chicares <gchicares@massmutual.com>
2008-08-19 13:40:33 +0000

    Improve a name that looks incorrect
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/lmi/lmi/trunk@4095 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- dbnames.hpp
+++ dbnames.hpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: dbnames.hpp,v 1.16 2008-08-19 12:38:40 chicares Exp $
+// $Id: dbnames.hpp,v 1.17 2008-08-19 13:40:32 chicares Exp $
 
 #ifndef dbnames_hpp
 #define dbnames_hpp
@@ -267,7 +267,7 @@ enum DatabaseNames
         ,DB_ADDPremRate
 
         ,DB_AllowSpouse
-        ,DB_SpousRiderGuarTable
+        ,DB_SpouseRiderGuarTable
         ,DB_SpouseRiderTable
 
         ,DB_AllowChild
