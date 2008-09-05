c93e97ae40bbfe7d5a522e5e54eea67ddb72e757
Gregory W. Chicares <chicares@sbcglobal.net>
2008-09-05 16:20:32 +0000

    Demonstrate use of PETE with std::vector

---
+++ tools/pete-2.1.1/et_vector.hpp
@@ -0,0 +1,61 @@
+// PETE with std::vector.
+//
+// Copyright (C) 2008 Gregory W. Chicares.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+//
+// http://savannah.nongnu.org/projects/lmi
+// email: <gchicares@sbcglobal.net>
+// snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
+
+// $Id: et_vector.hpp,v 1.1 2008-09-05 16:20:31 chicares Exp $
+
+#ifndef et_vector_hpp
+#define et_vector_hpp
+
+#if 0
+#include "config.hpp"
+#endif // 0
+
+// These headers must be included before "et_vector_operators.hpp"
+// because the latter doesn't include them.
+#include "PETE/PETE.h"
+#include <vector>
+
+#include "et_vector_operators.hpp"
+
+/// Create vector-iterator leaves.
+
+template<class T>
+struct CreateLeaf<std::vector<T> >
+{
+    typedef typename std::vector<T>::const_iterator Leaf_t;
+    static Leaf_t make(std::vector<T> const& v) {return v.begin();}
+};
+
+/// All PETE assignment operators call evaluate().
+
+template<class T, class Op, class U>
+inline void evaluate(std::vector<T>& t, Op const& op, U const& u)
+{
+    typedef typename std::vector<T>::iterator svi;
+    for(svi i = t.begin(); i != t.end(); ++i)
+        {
+        op(*i, forEach(u, DereferenceLeaf(), OpCombine()));
+        forEach(u, IncrementLeaf(), NullCombine());
+        }
+}
+
+#endif // et_vector_hpp
+
---
+++ tools/pete-2.1.1/et_vector_test.cpp
@@ -0,0 +1,43 @@
+// PETE with std::vector: unit test.
+//
+// Copyright (C) 2008 Gregory W. Chicares.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+//
+// http://savannah.nongnu.org/projects/lmi
+// email: <gchicares@sbcglobal.net>
+// snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
+
+// $Id: et_vector_test.cpp,v 1.1 2008-09-05 16:20:32 chicares Exp $
+
+#include "et_vector.hpp"
+
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+
+int main()
+{
+    std::vector<double> v0;
+
+    v0.push_back(0.0);
+    v0.push_back(1.1);
+    v0.push_back(2.2);
+
+    v0 *= v0;
+
+    std::ostream_iterator<double> osi(std::cout, "\n");
+    std::copy(v0.begin(), v0.end(), osi);
+}
+
