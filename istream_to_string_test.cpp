df0501aa915621fd3fef949ae0c6e92d40bfdc17
Greg Chicares <gchicares@massmutual.com>
2008-09-21 18:23:34 +0000

    Test various file sizes
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/lmi/lmi/trunk@4180 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- istream_to_string_test.cpp
+++ istream_to_string_test.cpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: istream_to_string_test.cpp,v 1.1 2008-09-21 13:09:29 chicares Exp $
+// $Id: istream_to_string_test.cpp,v 1.2 2008-09-21 18:23:34 chicares Exp $
 
 #ifdef __BORLANDC__
 #   include "pchfile.hpp"
@@ -70,19 +70,32 @@ void istream_to_string_1
         }
 }
 
+template<char* filename>
 void mete_0()
 {
-    std::ifstream ifs("sample.cns", ios_in_binary());
+    std::ifstream ifs(filename, ios_in_binary());
     std::string s;
     istream_to_string_0(ifs, s);
 }
 
+template<char* filename>
 void mete_1()
 {
-    std::ifstream ifs("sample.cns", ios_in_binary());
+    std::ifstream ifs(filename, ios_in_binary());
     std::string s;
     istream_to_string_1(ifs, s);
 }
+
+namespace
+{
+char f10      [] = "eraseme.10";
+char f100     [] = "eraseme.100";
+char f1000    [] = "eraseme.1000";
+char f10000   [] = "eraseme.10000";
+char f100000  [] = "eraseme.100000";
+char f1000000 [] = "eraseme.1000000";
+char f10000000[] = "eraseme.10000000";
+} // Unnamed namespace.
 
 int test_main(int, char*[])
 {
@@ -102,10 +115,45 @@ int test_main(int, char*[])
 
     // Test speed.
 
-    std::cout << "  Speed tests...\n"
-        << "  Method 0: " << TimeAnAliquot(mete_0) << '\n'
-        << "  Method 1: " << TimeAnAliquot(mete_1) << '\n'
+    std::string const digits("0123456789");
+    for(int i = 1; i < 10000000; i *= 10)
+        {
+        std::ostringstream oss;
+        oss << "eraseme." << i * digits.size();
+        std::ofstream ofs(oss.str().c_str(), ios_out_trunc_binary());
+        for(int j = 0; j < i; ++j)
+            {
+            ofs << digits;
+            }
+        }
+
+    std::cout << "\n  Speed tests for method 0...\n"
+        << "       10 bytes: " << TimeAnAliquot(mete_0<f10      >) << '\n'
+        << "      100 bytes: " << TimeAnAliquot(mete_0<f100     >) << '\n'
+        << "     1000 bytes: " << TimeAnAliquot(mete_0<f1000    >) << '\n'
+        << "    10000 bytes: " << TimeAnAliquot(mete_0<f10000   >) << '\n'
+        << "   100000 bytes: " << TimeAnAliquot(mete_0<f100000  >) << '\n'
+        << "  1000000 bytes: " << TimeAnAliquot(mete_0<f1000000 >) << '\n'
+        << " 10000000 bytes: " << TimeAnAliquot(mete_0<f10000000>) << '\n'
         ;
+
+    std::cout << "\n  Speed tests for method 1...\n"
+        << "       10 bytes: " << TimeAnAliquot(mete_1<f10      >) << '\n'
+        << "      100 bytes: " << TimeAnAliquot(mete_1<f100     >) << '\n'
+        << "     1000 bytes: " << TimeAnAliquot(mete_1<f1000    >) << '\n'
+        << "    10000 bytes: " << TimeAnAliquot(mete_1<f10000   >) << '\n'
+        << "   100000 bytes: " << TimeAnAliquot(mete_1<f100000  >) << '\n'
+        << "  1000000 bytes: " << TimeAnAliquot(mete_1<f1000000 >) << '\n'
+        << " 10000000 bytes: " << TimeAnAliquot(mete_1<f10000000>) << '\n'
+        ;
+
+    std::remove(f10      );
+    std::remove(f100     );
+    std::remove(f1000    );
+    std::remove(f10000   );
+    std::remove(f100000  );
+    std::remove(f1000000 );
+    std::remove(f10000000);
 
     return 0;
 }
