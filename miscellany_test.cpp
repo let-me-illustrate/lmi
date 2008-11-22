a240093ef6e449a84d971a6dbb5362d4cd3e72dc
Greg Chicares <gchicares@massmutual.com>
2008-11-22 14:43:29 +0000

    Add a unit test for a defect exposed 20081122T1241Z
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/lmi/lmi/trunk@4286 8995fb05-a277-41b4-a4d2-e84fd280a9a6

---
+++ miscellany_test.cpp
@@ -0,0 +1,46 @@
+// Miscellaneous functions--unit test.
+//
+// Copyright (C) 2008 Gregory W. Chicares.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+//
+// http://savannah.nongnu.org/projects/lmi
+// email: <chicares@cox.net>
+// snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
+
+// $Id: miscellany_test.cpp,v 1.1 2008-11-22 14:43:28 chicares Exp $
+
+#ifdef __BORLANDC__
+#   include "pchfile.hpp"
+#   pragma hdrstop
+#endif // __BORLANDC__
+
+#include "miscellany.hpp"
+
+#include "test_tools.hpp"
+
+void test_files_are_identical()
+{
+    std::string f0("unlikely_file_name_0");
+    std::string f1("unlikely_file_name_1");
+    BOOST_TEST(!files_are_identical(f0, f1));
+}
+
+int test_main(int, char*[])
+{
+    test_files_are_identical();
+
+    return 0;
+}
+
