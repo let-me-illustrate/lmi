2ae86c148a9aa92d95c485bf84926ab3d8fb284a
Greg Chicares <gchicares@massmutual.com>
2008-11-25 16:16:35 +0000

    Add a function to make a filename portable
    git-svn-id: svn+ssh://svn.savannah.nongnu.org/lmi/lmi/trunk@4300 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- path_utility.hpp
+++ path_utility.hpp
@@ -19,7 +19,7 @@
 // email: <chicares@cox.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: path_utility.hpp,v 1.13 2008-11-24 18:13:14 chicares Exp $
+// $Id: path_utility.hpp,v 1.14 2008-11-25 16:16:35 chicares Exp $
 
 #ifndef path_utility_hpp
 #define path_utility_hpp
@@ -32,7 +32,11 @@
 
 #include <string>
 
-void LMI_SO initialize_filesystem();
+void LMI_SO initialize_filesystem(); // Listed first because of its importance.
+
+std::string LMI_SO orthodox_filename
+    (std::string const& original_filename
+    );
 
 std::string LMI_SO serial_extension
     (int                serial_number
