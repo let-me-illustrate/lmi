613cb5d327cf2080121d470c64de4f2ba60bd93f
Gregory W. Chicares <chicares@sbcglobal.net>
2009-03-09 00:09:46 +0000

    Rearrange

--- input_harmonization.cpp
+++ input_harmonization.cpp
@@ -19,7 +19,7 @@
 // email: <gchicares@sbcglobal.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: input_harmonization.cpp,v 1.93 2009-03-08 20:14:45 chicares Exp $
+// $Id: input_harmonization.cpp,v 1.94 2009-03-09 00:09:46 chicares Exp $
 
 #ifdef __BORLANDC__
 #   include "pchfile.hpp"
@@ -161,17 +161,6 @@ void Input::DoHarmonize()
             << LMI_FLUSH
             ;
         }
-
-    bool enable_reduce_to_avoid_mec = mce_solve_for_non_mec != SolveTarget;
-/* TODO ?? Want something like this?
-    bool enable_reduce_to_avoid_mec =
-        (
-            SpecifiedAmountStrategyFromIssue == mce_sainputscalar
-        ||  SpecifiedAmountStrategyFromIssue == mce_sainputvector
-        ||  SpecifiedAmountStrategyFromIssue == mce_sa_salary
-        );
-*/
-    AvoidMecMethod.allow(mce_reduce_prem, enable_reduce_to_avoid_mec);
 
     MaximumNaar.enable(anything_goes);
 
@@ -876,6 +865,17 @@ false // Silly workaround for now.
     SolveSeparateAccountBasis.allow(mce_sep_half, actually_solving && allow_sep_acct && is_three_rate_nasd(GleanedLedgerType_));
 
     SolveTargetCashSurrenderValue.enable(actually_solving && mce_solve_for_target == SolveTarget);
+
+    bool enable_reduce_to_avoid_mec = mce_solve_for_non_mec != SolveTarget;
+/* TODO ?? Want something like this?
+    bool enable_reduce_to_avoid_mec =
+        (
+            SpecifiedAmountStrategyFromIssue == mce_sainputscalar
+        ||  SpecifiedAmountStrategyFromIssue == mce_sainputvector
+        ||  SpecifiedAmountStrategyFromIssue == mce_sa_salary
+        );
+*/
+    AvoidMecMethod.allow(mce_reduce_prem, enable_reduce_to_avoid_mec);
 
     bool create_supplemental_report = "Yes" == CreateSupplementalReport;
     SupplementalReportColumn00.enable(create_supplemental_report);
