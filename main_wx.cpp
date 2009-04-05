2999bcb5ee79c4f2f8c703a7010dd006309e71ca
Greg Chicares <gchicares@massmutual.com>
2009-04-05 10:33:28 +0000

    Fix defect introduced 20090330T1723Z
    git-svn-id: svn://svn.sv.gnu.org/lmi/lmi/trunk@4534 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- main_wx.cpp
+++ main_wx.cpp
@@ -19,7 +19,7 @@
 // email: <gchicares@sbcglobal.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id: main_wx.cpp,v 1.135 2009-03-31 17:01:14 chicares Exp $
+// $Id: main_wx.cpp,v 1.136 2009-04-05 10:33:28 chicares Exp $
 
 // Portions of this file are derived from wxWindows files
 //   samples/docvwmdi/docview.cpp (C) 1998 Julian Smart and Markus Holzem
@@ -634,7 +634,7 @@ bool Skeleton::OnInit()
 
         DefaultView const v0;
 #if wxCHECK_VERSION(2,9,0)
-        if(!xml_resources.LoadFile(AddDataDir(v0.ResourceFileName())))
+        if(!xml_resources.LoadFile(wxFileName(AddDataDir(v0.ResourceFileName()))))
 #else  // !wxCHECK_VERSION(2,9,0)
         if(!xml_resources.Load(AddDataDir(v0.ResourceFileName())))
 #endif // !wxCHECK_VERSION(2,9,0)
@@ -644,7 +644,7 @@ bool Skeleton::OnInit()
 
         PreferencesView const v1;
 #if wxCHECK_VERSION(2,9,0)
-        if(!xml_resources.LoadFile(AddDataDir(v1.ResourceFileName())))
+        if(!xml_resources.LoadFile(wxFileName(AddDataDir(v1.ResourceFileName()))))
 #else  // !wxCHECK_VERSION(2,9,0)
         if(!xml_resources.Load(AddDataDir(v1.ResourceFileName())))
 #endif // !wxCHECK_VERSION(2,9,0)
@@ -653,7 +653,7 @@ bool Skeleton::OnInit()
             }
 
 #if wxCHECK_VERSION(2,9,0)
-        if(!xml_resources.LoadFile(AddDataDir("menus.xrc")))
+        if(!xml_resources.LoadFile(wxFileName(AddDataDir("menus.xrc"))))
 #else  // !wxCHECK_VERSION(2,9,0)
         if(!xml_resources.Load(AddDataDir("menus.xrc")))
 #endif // !wxCHECK_VERSION(2,9,0)
@@ -662,7 +662,7 @@ bool Skeleton::OnInit()
             }
 
 #if wxCHECK_VERSION(2,9,0)
-        if(!xml_resources.LoadFile(AddDataDir("toolbar.xrc")))
+        if(!xml_resources.LoadFile(wxFileName(AddDataDir("toolbar.xrc"))))
 #else  // !wxCHECK_VERSION(2,9,0)
         if(!xml_resources.Load(AddDataDir("toolbar.xrc")))
 #endif // !wxCHECK_VERSION(2,9,0)
@@ -671,7 +671,7 @@ bool Skeleton::OnInit()
             }
 
 #if wxCHECK_VERSION(2,9,0)
-        if(!xml_resources.LoadFile(AddDataDir(PolicyView::resource_file_name())))
+        if(!xml_resources.LoadFile(wxFileName(AddDataDir(PolicyView::resource_file_name()))))
 #else  // !wxCHECK_VERSION(2,9,0)
         if(!xml_resources.Load(AddDataDir(PolicyView::resource_file_name())))
 #endif // !wxCHECK_VERSION(2,9,0)
@@ -680,7 +680,7 @@ bool Skeleton::OnInit()
             }
 
 #if wxCHECK_VERSION(2,9,0)
-        if(!xml_resources.LoadFile(AddDataDir(RoundingView::resource_file_name())))
+        if(!xml_resources.LoadFile(wxFileName(AddDataDir(RoundingView::resource_file_name()))))
 #else  // !wxCHECK_VERSION(2,9,0)
         if(!xml_resources.Load(AddDataDir(RoundingView::resource_file_name())))
 #endif // !wxCHECK_VERSION(2,9,0)
