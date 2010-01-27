e1f559cd066276a21621e8e9df24a05241a567f3
Wendy Boutin <wboutin@massmutual.com>
2010-01-27 16:15:20 +0000

    Mark release candidate
    git-svn-id: svn://svn.sv.gnu.org/lmi/lmi/trunk@4768 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- version.hpp
+++ version.hpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2009, 2010 Gregory W. Chicares.
+// Copyright (C) 2010 Gregory W. Chicares.
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License version 2 as
@@ -24,7 +24,7 @@
 
 #include "config.hpp"
 
-#define LMI_VERSION "20090929T0121Z"
+#define LMI_VERSION "20100127T1613Z"
 
 #endif // version_hpp
 
