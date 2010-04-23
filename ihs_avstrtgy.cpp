5c2b3f27d8cab87fc815a5a9fd5804a17db6db37
Gregory W. Chicares <chicares@sbcglobal.net>
2010-04-23 22:28:24 +0000

    Remove unneeded headers

--- ihs_avstrtgy.cpp
+++ ihs_avstrtgy.cpp
@@ -30,9 +30,7 @@
 
 #include "alert.hpp"
 #include "death_benefits.hpp"
-#include "ihs_rnddata.hpp"
 #include "ledger_invariant.hpp"
-#include "ledger_variant.hpp"
 #include "mortality_rates.hpp"
 
 #include <algorithm>
@@ -50,9 +48,6 @@
 //
 // There are two functions for specamt strategy; apparently only the one
 // called "Old" is used.
-//
-// Why is VariantValues used? Where it is used, wouldn't it be clearer
-// just to get the initial values from BasicValues or InputParms?
 
 //============================================================================
 double AccountValue::CalculateSpecAmtFromStrategy
