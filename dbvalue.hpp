9e076612cdbab6c314b6ddd855f4bfe69728823d
Greg Chicares <gchicares@massmutual.com>
2010-05-03 11:48:28 +0000

    Prefer implicitly-defined special member functions [12/1]
    git-svn-id: svn://svn.sv.gnu.org/lmi/lmi/trunk@4896 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- dbvalue.hpp
+++ dbvalue.hpp
@@ -53,6 +53,8 @@ namespace xml_serialize {template<typename T> struct xml_io;}
 /// all axes. In a typical query, all other axes are single-valued,
 /// but all durations are wanted; this axis ordering puts consecutive
 /// durational values in contiguous storage for efficient retrieval.
+///
+/// Implicitly-declared special member functions do the right thing.
 
 class LMI_SO TDBValue
     :virtual private obstruct_slicing<TDBValue>
@@ -91,8 +93,6 @@ class LMI_SO TDBValue
         ,double             datum
         ,std::string const& gloss = std::string()
         );
-    TDBValue(TDBValue const&);
-    TDBValue& operator=(TDBValue const&);
     ~TDBValue();
 
     double const* operator[](TDBIndex const& idx) const;
