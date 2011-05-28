d32538b0bf83bf9ac4e8f0b0e337ae470c809b73
Greg Chicares <gchicares@massmutual.com>
2011-05-28 11:24:13 +0000

    Add inchoate premium-tax class with unit test
    git-svn-id: svn://svn.sv.gnu.org/lmi/lmi/trunk@5182 8995fb05-a277-41b4-a4d2-e84fd280a9a6

---
+++ premium_tax.hpp
@@ -0,0 +1,54 @@
+// Premium tax.
+//
+// Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011 Gregory W. Chicares.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+//
+// http://savannah.nongnu.org/projects/lmi
+// email: <gchicares@sbcglobal.net>
+// snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
+
+// $Id$
+
+#ifndef premium_tax_hpp
+#define premium_tax_hpp
+
+#include "config.hpp"
+
+#include "mc_enum_type_enums.hpp" // mcenum_state
+#include "obstruct_slicing.hpp"
+#include "uncopyable_lmi.hpp"
+
+class product_database;
+class stratified_charges;
+
+class premium_tax
+  :        private lmi::uncopyable <premium_tax>
+  ,virtual private obstruct_slicing<premium_tax>
+{
+  public:
+    premium_tax
+        (mcenum_state              premium_tax_state
+        ,mcenum_state              state_of_domicile
+        ,bool                      amortize_premium_load
+        ,product_database   const& database
+        ,stratified_charges const& strata
+        );
+    ~premium_tax();
+
+  private:
+};
+
+#endif // premium_tax_hpp
+
---
+++ premium_tax_test.cpp
@@ -0,0 +1,61 @@
+// Premium tax--unit test.
+//
+// Copyright (C) 2011 Gregory W. Chicares.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+//
+// http://savannah.nongnu.org/projects/lmi
+// email: <gchicares@sbcglobal.net>
+// snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
+
+// $Id$
+
+#ifdef __BORLANDC__
+#   include "pchfile.hpp"
+#   pragma hdrstop
+#endif // __BORLANDC__
+
+#include "premium_tax.hpp"
+
+#include "path_utility.hpp" // initialize_filesystem()
+#include "test_tools.hpp"
+
+class premium_tax_test
+{
+  public:
+    static void test()
+        {
+        test_something();
+        }
+
+  private:
+    static void test_something();
+};
+
+/// Placeholder.
+
+void premium_tax_test::test_something()
+{
+}
+
+int test_main(int, char*[])
+{
+    // Absolute paths require "native" name-checking policy for msw.
+    initialize_filesystem();
+
+    premium_tax_test::test();
+
+    return EXIT_SUCCESS;
+}
+
