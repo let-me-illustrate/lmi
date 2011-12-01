5b8c1ca3880ed0566e5d27fea577b73f2f5d3b62
Greg Chicares <gchicares@massmutual.com>
2011-12-01 13:02:30 +0000

    Improve documentation, adumbrating some planned changes
    git-svn-id: svn://svn.sv.gnu.org/lmi/lmi/trunk@5321 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- ihs_basicval.cpp
+++ ihs_basicval.cpp
@@ -663,6 +663,14 @@ void BasicValues::Init7702A()
 }
 
 //============================================================================
+// [This comment block will soon be expunged. This function is called
+// only by FindSpecAmt::operator(), but that GPT calculation could
+// just as well call GetModalTgtPrem() directly. No product design
+// currently supported has a target premium that varies by dbopt,
+// so at most a "SOMEDAY" marker is warranted. The caching of
+// InitialTargetPremium is unreliable if this function isn't first
+// called with an a_year of zero; it's meaningless anyway, because
+// the function happens to be called only with an a_year of zero.]
 // Needed for guideline premium.
 // TODO ?? a_dbopt is ignored for now, but some product designs will need it.
 double BasicValues::GetTgtPrem
@@ -672,6 +680,7 @@ double BasicValues::GetTgtPrem
     ,mcenum_mode  a_mode
     ) const
 {
+LMI_ASSERT(0 == a_year); // As noted above.
     if(Database_->Query(DB_TgtPremFixedAtIssue))
         {
         if(0 == a_year)
