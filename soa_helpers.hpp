2c075acc74c3dedee2043dda2077bf3962d53fe4
Gregory W. Chicares <chicares@sbcglobal.net>
2012-05-27 16:36:25 +0000

    Fix 'make check_concinnity' issues in files just added

--- soa_helpers.hpp
+++ soa_helpers.hpp
@@ -1,3 +1,30 @@
+// Auxiliary routines for tables in deprecated SOA format.
+//
+// Copyright (C) 2012 Gregory W. Chicares.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+//
+// http://savannah.nongnu.org/projects/lmi
+// email: <gchicares@sbcglobal.net>
+// snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
+
+// $Id$
+
+#ifndef soa_helpers_hpp
+#define soa_helpers_hpp
+
+#include "config.hpp"
 
 #include "actuarial_table.hpp"
 #include "miscellany.hpp"
@@ -16,11 +43,10 @@
  misc helpers
  ************************************************************************/
 
-inline void error(const boost::format& fmt)
+inline void error(boost::format const& fmt)
 {
     throw std::runtime_error(fmt.str());
 }
-
 
 /************************************************************************
  SOA actuarial table format helpers
@@ -32,7 +58,7 @@ struct soa_record_info
     std::string name;
 };
 
-std::vector<soa_record_info> list_soa_file_tables(const char *filename)
+std::vector<soa_record_info> list_soa_file_tables(char const* filename)
 {
     std::vector<soa_record_info> v;
 
@@ -84,4 +110,6 @@ std::vector<soa_record_info> list_soa_file_tables(const char *filename)
 
     return v;
 }
+
+#endif // soa_helpers_hpp
 
