cc0a25a7ac2724b54df6887ebab2069379a1b870
Václav Slavík <vaclav@tt-solutions.com>
2012-08-01 19:29:28 +0200

    Report missing files in loaded_files_cache<>.

--- loaded_files_cache.hpp
+++ loaded_files_cache.hpp
@@ -26,6 +26,7 @@
 
 #include "config.hpp"
 
+#include "alert.hpp"
 #include "uncopyable_lmi.hpp"
 
 #include <boost/filesystem/operations.hpp>
@@ -56,30 +57,42 @@ class loaded_files_cache
     // if it was modified since the cached copy was read.
     value_ptr_type get(key_type const& filename)
         {
-        std::time_t const write_time = fs::last_write_time(filename);
-
-        typename map_type::iterator i = cache_.lower_bound(filename);
-        if
-            (  i != cache_.end()
-            && i->first == filename
-            && write_time == i->second.write_time
-            )
+        try
             {
-            return i->second.data;
+            std::time_t const write_time = fs::last_write_time(filename);
+
+            typename map_type::iterator i = cache_.lower_bound(filename);
+            if
+                (  i != cache_.end()
+                && i->first == filename
+                && write_time == i->second.write_time
+                )
+                {
+                return i->second.data;
+                }
+
+            // create the value before calling insert() because the call may throw
+            value_ptr_type value(new value_type(filename));
+
+            // insert() doesn't update the value if the key is already present, so
+            // insert a dummy value and then modify it -- this will work for both
+            // existing and new keys.
+            i = cache_.insert(i, typename map_type::value_type(filename, record()));
+            record& rec = i->second;
+            rec.write_time = write_time;
+            rec.data = value;
+
+            return value;
             }
-
-        // create the value before calling insert() because the call may throw
-        value_ptr_type value(new value_type(filename));
-
-        // insert() doesn't update the value if the key is already present, so
-        // insert a dummy value and then modify it -- this will work for both
-        // existing and new keys.
-        i = cache_.insert(i, typename map_type::value_type(filename, record()));
-        record& rec = i->second;
-        rec.write_time = write_time;
-        rec.data = value;
-
-        return value;
+        catch(fs::filesystem_error const&)
+            {
+            fatal_error()
+                << "File '"
+                << filename
+                << "' is required but could not be found. Try reinstalling."
+                << LMI_FLUSH
+                ;
+            }
         }
 
     // Return singleton instance of the cache.
