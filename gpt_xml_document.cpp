9cc702a51196fa14f926a116caf0a40d0bbee2a3
Greg Chicares <gchicares@massmutual.com>
2014-06-15 17:17:49 +0000

    Clone 'mec*' source files for GPT
    git-svn-id: svn://svn.sv.gnu.org/lmi/lmi/trunk@5893 8995fb05-a277-41b4-a4d2-e84fd280a9a6

---
+++ gpt_xml_document.cpp
@@ -0,0 +1,100 @@
+// xml document for guideline premium test.
+//
+// Copyright (C) 2009, 2010, 2011, 2012, 2013, 2014 Gregory W. Chicares.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+//
+// http://savannah.nongnu.org/projects/lmi
+// email: <gchicares@sbcglobal.net>
+// snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
+
+// $Id$
+
+#ifdef __BORLANDC__
+#   include "pchfile.hpp"
+#   pragma hdrstop
+#endif // __BORLANDC__
+
+#include "gpt_xml_document.hpp"
+
+#include "assert_lmi.hpp"
+#include "xml_lmi.hpp"
+
+#include <xmlwrapp/nodes_view.h>
+
+#include <istream>
+#include <ostream>
+
+//============================================================================
+gpt_xml_document::gpt_xml_document()
+    :input_data_()
+{
+}
+
+//============================================================================
+gpt_xml_document::gpt_xml_document(gpt_input const& z)
+    :input_data_(z)
+{
+}
+
+//============================================================================
+gpt_xml_document::gpt_xml_document(std::string const& filename)
+    :input_data_()
+{
+    xml_lmi::dom_parser parser(filename);
+    parse(parser);
+}
+
+//============================================================================
+gpt_xml_document::~gpt_xml_document()
+{
+}
+
+//============================================================================
+std::string const& gpt_xml_document::xml_root_name() const
+{
+    static std::string const s("gpt_xml_document");
+    return s;
+}
+
+//============================================================================
+void gpt_xml_document::parse(xml_lmi::dom_parser const& parser)
+{
+    xml::element const& root(parser.root_node(xml_root_name()));
+    xml::const_nodes_view const elements(root.elements());
+    LMI_ASSERT(!elements.empty());
+    xml::const_nodes_view::const_iterator i(elements.begin());
+    *i >> input_data_;
+    // XMLWRAPP !! It would be better to have operator+(int) in the
+    // iterator class, and to write this check above as
+    //   LMI_ASSERT(elements.end() == 1 + i);
+    LMI_ASSERT(elements.end() == ++i);
+}
+
+//============================================================================
+void gpt_xml_document::read(std::istream const& is)
+{
+    xml_lmi::dom_parser parser(is);
+    parse(parser);
+}
+
+//============================================================================
+void gpt_xml_document::write(std::ostream& os) const
+{
+    xml_lmi::xml_document document(xml_root_name());
+    xml::element& root = document.root_node();
+    root << input_data_;
+    os << document;
+}
+
---
+++ gpt_xml_document.hpp
@@ -0,0 +1,69 @@
+// xml document for guideline premium test.
+//
+// Copyright (C) 2009, 2010, 2011, 2012, 2013, 2014 Gregory W. Chicares.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+//
+// http://savannah.nongnu.org/projects/lmi
+// email: <gchicares@sbcglobal.net>
+// snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
+
+// $Id$
+
+#ifndef gpt_xml_document_hpp
+#define gpt_xml_document_hpp
+
+#include "config.hpp"
+
+#include "gpt_input.hpp"
+#include "obstruct_slicing.hpp"
+#include "so_attributes.hpp"
+#include "uncopyable_lmi.hpp"
+#include "xml_lmi_fwd.hpp"
+
+#include <iosfwd>
+#include <string>
+
+class LMI_SO gpt_xml_document
+    :        private lmi::uncopyable <gpt_xml_document>
+    ,virtual private obstruct_slicing<gpt_xml_document>
+{
+    friend class gpt_document;
+    friend class gpt_view;
+
+  public:
+    gpt_xml_document();
+    gpt_xml_document(gpt_input const&);
+    gpt_xml_document(std::string const& filename);
+    ~gpt_xml_document();
+
+    gpt_input const& input_data() const;
+
+    void read(std::istream const&);
+    void write(std::ostream&) const;
+
+  private:
+    void parse(xml_lmi::dom_parser const&);
+    std::string const& xml_root_name() const;
+
+    gpt_input input_data_;
+};
+
+inline gpt_input const& gpt_xml_document::input_data() const
+{
+    return input_data_;
+}
+
+#endif // gpt_xml_document_hpp
+
