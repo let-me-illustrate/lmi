ca208d612dfa645cc406fa1b9d63d48d60055ae8
Gregory W. Chicares <chicares@sbcglobal.net>
2014-07-09 23:00:25 +0000

    Rectify names

--- ihs_server7702io.hpp
+++ ihs_server7702io.hpp
@@ -38,43 +38,43 @@ struct Server7702Input
     Server7702Input(gpt_input const&);
     operator gpt_input() const;
 
-    std::string      UniqueIdentifier;           //  an arbitrary string that identifies the contract uniquely. The contract number may be used, but remember that the server maintains no database of actual contracts.
+    std::string      ContractNumber;             //  an arbitrary string that identifies the contract uniquely. The contract number may be used, but remember that the server maintains no database of actual contracts.
     bool             IsIssuedToday;              //  true if the contract is issued or reissued today. Used to prevent adjustable events at issue, which must not occur.
-    int              Duration;                   //  number of policy years completed since issue (so it starts at 0).
-    double           GrossNontaxableWithdrawal;  // UNUSED IN OLD TESTDECK the nontaxable portion of partial surrenders, including any withdrawal fees, plus involuntary withdrawals to restrict NAAR for reinsurance, plus amounts returned to preserve §7702A status.
-    double           Premium;                    // UNUSED IN OLD TESTDECK gross payments, including those paid by a waiver benefit, before subtracting any "GrossNontaxableWithdrawal" on the same date, but net of any charges for non-qualified additional benefits that are not prefunded.
+    int              InforceYear;                //  number of policy years completed since issue (so it starts at 0).
+    double           PremsPaidDecrement;         // UNUSED IN OLD TESTDECK the nontaxable portion of partial surrenders, including any withdrawal fees, plus involuntary withdrawals to restrict NAAR for reinsurance, plus amounts returned to preserve §7702A status.
+    double           Payment;                    // UNUSED IN OLD TESTDECK gross payments, including those paid by a waiver benefit, before subtracting any "PremsPaidDecrement" on the same date, but net of any charges for non-qualified additional benefits that are not prefunded.
     double           DecreaseRequiredByContract; // UNUSED amount of decrease in specified amount required by the contract's terms, as for example in a decreasing term contract: unsupported, so use 0.0 for now.
     std::string      ProductName;                //  the only permissible values are those for which data files exist.
-    mce_uw_basis     UnderwritingBasis;          //  permissible values are Medical, Paramedical, Nonmedical, Simplified_issue, and Guaranteed_issue, and I assume this is set at issue and can never change.
+    mce_uw_basis     GroupUnderwritingType;      //  permissible values are Medical, Paramedical, Nonmedical, Simplified_issue, and Guaranteed_issue, and I assume this is set at issue and can never change.
     double           PremTaxLoadRate;            //  the rate actually charged as a premium load. Example: 0.02 means a 2% load. Used to determine whether any load reflected in §7702 calculations has changed.
     double           TieredAssetChargeRate;      // UNUSED the tiered rate actually charged against assets, reflecting the current tier. Example: 0.0050 means fifty basis points. Used to determine whether any load reflected in §7702 calculations has changed.
     double           LeastBenefitAmountEver;     // UNUSED the lowest face amount ever used in §7702 calculations since the contract was issued; equal at issue to DB. The server will always calculate this but never store it. The client will always store it but never calculate it, except to set it equal to DB at issue.
 
     // The names of the next several parameters begin with the prefix "Old" to indicate that they are evaluated before the day's transactions. Should an adjustable event occur, they describe the state of the contract before any transaction that might have caused the adjustable event. At issue, "Old" values are the contract's issue parameters.
-    double           OldGuidelineLevelPremium;   //  as previously calculated by the server; 0.0 at issue.
-    double           OldGuidelineSinglePremium;  //  as previously calculated by the server; 0.0 at issue.
+    double           InforceGlp;                 //  as previously calculated by the server; 0.0 at issue.
+    double           InforceGsp;                 //  as previously calculated by the server; 0.0 at issue.
 
     // Each of the remaining parameters must be supplied in both a "new" and an "old" flavor, reflecting the state of the contract respectively before and after the day's transactions. For economy of presentation I don't repeat the list here with "Old" and "New" prefixes, but we'll have to do that when specifying the record layout.
     int              NewIssueAge;                //  as defined in the GPT specs.
     int              OldIssueAge;                // UNUSED IN OLD TESTDECK
     mce_gender       NewGender;                  //  male, female, or unisex.
     mce_gender       OldGender;                  // UNUSED IN OLD TESTDECK
-    mce_smoking      NewSmoker;                  //  smoker, nonsmoker, or unismoke.
-    mce_smoking      OldSmoker;
+    mce_smoking      NewSmoking;                 //  smoker, nonsmoker, or unismoke.
+    mce_smoking      OldSmoking;
 
     mce_class        NewUnderwritingClass;       //  permissible values are Preferred, Standard, and Rated.
     mce_class        OldUnderwritingClass;       // UNUSED IN OLD TESTDECK
     mce_state        NewStateOfJurisdiction;     //  capitalized two-letter postal abbreviation for the state that determines the premium tax rate.
     mce_state        OldStateOfJurisdiction;     // UNUSED IN OLD TESTDECK
 
-    mce_dbopt_7702   NewDeathBenefitOption;      //  DBO--A or B only.
-    mce_dbopt_7702   OldDeathBenefitOption;
+    mce_dbopt_7702   NewDbo;                     //  DBO--A or B only.
+    mce_dbopt_7702   OldDbo;
     double           NewBenefitAmount;           //  'death benefit' as defined in 7702: we are using the amount payable by reason of death.
     double           OldBenefitAmount;           //  the benefit amount before today's transactions; at issue, the amount described in the section on dumpins.
-    double           NewSpecifiedAmount;         //  SA.
-    double           OldSpecifiedAmount;
-    double           NewTermAmount;              //  benefit amount of any term rider; 0.0 if none.
-    double           OldTermAmount;
+    double           NewSpecAmt;                 //  SA.
+    double           OldSpecAmt;
+    double           NewQabTermAmt;              //  benefit amount of any term rider; 0.0 if none.
+    double           OldQabTermAmt;
     bool             NewWaiverOfPremiumInForce;  // UNUSED true if waiver benefit in force, otherwise false.
     bool             OldWaiverOfPremiumInForce;  // UNUSED
     bool             NewPremiumsWaived;          // UNUSED true if waiver benefit in disability status, otherwise false.
@@ -85,8 +85,8 @@ struct Server7702Input
     bool             OldAccidentalDeathInForce;
     mce_table_rating NewAccidentalDeathRating;   // UNUSED substandard rating for premium waiver; I don't know what the permissible values are.
     mce_table_rating OldAccidentalDeathRating;   // UNUSED
-    mce_table_rating NewTableRating;             // UNUSED
-    mce_table_rating OldTableRating;             // UNUSED
+    mce_table_rating NewSubstandardTable;        // UNUSED
+    mce_table_rating OldSubstandardTable;        // UNUSED
 
     // The remaining parameters specify flat extras.
     // The distinction between temporary and permanent flats is important. I give the names with a suffix "0" here, but they need to be repeated with "1", "2", and so on for as many flats as permitted (how many is that?). As noted above, each has both an "old" and a "new" flavor.
@@ -118,7 +118,7 @@ struct Server7702Input
 
 struct Server7702Output
 {
-    std::string      UniqueIdentifier;              // the same identifier supplied as input.
+    std::string      ContractNumber;                // the same identifier supplied as input.
     int              Status;
     bool             AdjustableEventOccurred;
     double           GuidelineLevelPremium;         // the new GLP.
