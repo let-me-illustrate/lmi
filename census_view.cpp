10de7dba12d910cfbde4e6ee935ebad1b03442b7
Vadim Zeitlin <vadim@tt-solutions.com>
2015-02-15 15:45:58 +0100

    Micro optimization in CensusView::column_value_varies_across_cells().
    Move an invariant value out of the loop to speed up this function by ~25%.

--- census_view.cpp
+++ census_view.cpp
@@ -972,10 +972,12 @@ bool CensusView::column_value_varies_across_cells
     ,std::vector<Input> const& cells
     ) const
 {
+    const any_member<Input>& case_value = case_parms()[0][header];
+
     typedef std::vector<Input>::const_iterator ici;
     for(ici j = cells.begin(); j != cells.end(); ++j)
         {
-        if(!((*j)[header] == case_parms()[0][header]))
+        if(!((*j)[header] == case_value))
             {
             return true;
             }
