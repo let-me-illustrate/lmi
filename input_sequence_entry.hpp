523a07f83bd9d6aa36ba627cde738e34891cf6e2
Vadim Zeitlin <vadim@tt-solutions.com>
2015-11-19 01:40:33 +0100

    Allow opening editor from keyboard in InputSequenceEntry
    Handle Alt-Enter in InputSequenceTextCtrl as a shortcut for opening the editor
    dialog to allow doing it from keyboard even when InputSequenceTextCtrl is
    embedded in wxDataViewCtrl (as it happens when it's used in CensusView) and
    TAB-bing to the button is impossible as TAB presses are intercepted by
    wxDataViewCtrl to pass to the next cell.
    
    Additionally, using Alt-Enter is simpler than using Tab,Enter even when the
    control is used standalone.

--- input_sequence_entry.hpp
+++ input_sequence_entry.hpp
@@ -57,7 +57,10 @@ class InputSequenceEntry
 
   private:
     void UponChildKillFocus(wxFocusEvent&);
+    void UponEnter(wxCommandEvent&);
     void UponOpenEditor(wxCommandEvent&);
+
+    void DoOpenEditor();
 
     Input const* input_;
     std::string field_name_;
