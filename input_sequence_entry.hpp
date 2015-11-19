88f6e9d4aa30b49509fc108dbb269e8718e4cd28
Vadim Zeitlin <vadim@tt-solutions.com>
2015-11-19 01:38:00 +0100

    Handle focus loss in InputSequenceEntry itself
    Replace wxEVT_KILL_FOCUS handlers in InputSequenceTextCtrl and
    InputSequenceButton classes with a single handler for both of them in
    InputSequenceEntry.
    
    This slightly simplifies the code and also ensures that the more
    correct logic in the button event handler is used for the text control
    as well, so that things will work correctly if the editor can ever be
    shown while the text control has focus (which will eventually happen).

--- input_sequence_entry.hpp
+++ input_sequence_entry.hpp
@@ -54,6 +54,7 @@ class InputSequenceEntry
     void set_popup_title(wxString const& title) {title_ = title;}
 
   private:
+    void UponChildKillFocus(wxFocusEvent&);
     void UponOpenEditor(wxCommandEvent&);
 
     Input const* input_;
