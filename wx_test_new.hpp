22186ebb409e26a4dbd2cd85fd3fe70db7342451
Greg Chicares <gchicares@massmutual.com>
2015-12-13 22:00:46 +0000

    Update copyright notices
    git-svn-id: svn://svn.sv.gnu.org/lmi/lmi/trunk@6443 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- wx_test_new.hpp
+++ wx_test_new.hpp
@@ -1,6 +1,6 @@
 // Helper for creating new documents in unattended GUI tests.
 //
-// Copyright (C) 2014, 2015 Gregory W. Chicares.
+// Copyright (C) 2014, 2015, 2016 Gregory W. Chicares.
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License version 2 as
