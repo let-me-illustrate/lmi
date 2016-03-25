9c4a1fe02237fc93954dec3d68e78430988842b5
Greg Chicares <gchicares@massmutual.com>
2016-03-25 12:24:12 +0000

    Correct an obvious cut-and-pasto (VZ)
    git-svn-id: svn://svn.sv.gnu.org/lmi/lmi/trunk@6526 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- getopt.cpp
+++ getopt.cpp
@@ -918,8 +918,8 @@ GetOpt::usage(std::ostream& os)
     str_vec_i n;
     str_vec_i d;
     for
-        (n  = option_names.begin() ,d  = option_descriptions.begin()
-        ;n != option_names.end()   ,d != option_descriptions.end()
+        (n  = option_names.begin()  ,d  = option_descriptions.begin()
+        ;n != option_names.end()  && d != option_descriptions.end()
         ;++n, ++d
         )
         {
