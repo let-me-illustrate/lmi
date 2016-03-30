b00478a3250227350465b443525d445e7fa4754b
Gregory W. Chicares <chicares@sbcglobal.net>
2016-03-30 21:35:23 +0000

    Remove unneeded statusbar workarounds (VZ)

--- docmdichildframe_ex.hpp
+++ docmdichildframe_ex.hpp
@@ -21,45 +21,7 @@
 
 // $Id$
 
-// Enhance class wxDocMDIChildFrame to support MDI without requiring
-// the parent MDI frame window to display a 'Window' menu when it has
-// no MDI child, as recommended in msw platform documentation. This
-// implementation is rather specific to that platform.
-
-// The wx implementation of OnMenuHighlight() assumes that MDI child
-// frames have statusbars, but that practice is uncommon, at least for
-// msw, where typically only the MDI parent frame has a statusbar.
-// Instead of copying implementation details of OnMenuHighlight()
-// here, GetStatusBar() is overridden to change its behavior when it
-// is called in the menu-highlighting context. The new behavior shunts
-// menu highlighting to the parent frame's statusbar iff the child
-// frame has none. 'status_bar_sought_from_menu_highlight_handler_' is
-// a mode flag used to accomplish this. A more straightforward patch
-// to the library could be devised.
-
-// WX !! Even without these changes, handling enablement of child-frame
-// menuitems takes a bit of work. For example, change 'samples/mdi' by
-// adding EVT_MENU_OPEN handlers for both parent and child that call
-//   event.GetMenu()
-//   event.GetEventObject()
-// and report their values. All menus on the menubar report the parent
-// as the event object, but the 'window menu' and child menus return
-// null from GetMenu(). The child's system menu, however, reports the
-// child as its event object. Now add EVT_UPDATE_UI handlers for the
-// MDI_ABOUT item, for both parent and child: they are triggered only
-// by activating the respective system menu. Alternatively, add
-// EVT_UPDATE_UI handlers for MDI_CHILD_QUIT: only the child's handler
-// is triggered, and only when its system menu is activated. None of
-// these EVT_UPDATE_UI handlers can be triggered by manipulating the
-// menubars that own the menuitems corresponding to these events. At
-// least in these cases, the wx documentation for wxUpdateUIEvent:
-//   "On Windows and GTK+, events for menubar items are only sent when
-//   the menu is about to be shown, and not in idle time"
-// could be clarified.
-//
-// It seems satisfactory, however, to catch EVT_MENU_OPEN, in the
-// parent class only, with a handler that manages either parent or
-// child menuitems depending on whether GetActiveChild() returns null.
+// This will soon be removed because it no longer serves any purpose.
 
 #ifndef docmdichildframe_ex_hpp
 #define docmdichildframe_ex_hpp
@@ -88,14 +50,6 @@ class DocMDIChildFrameEx
         );
 
   private:
-    // WX !! Shouldn't OnMenuHighlight() be virtual?
-    void UponMenuHighlight(wxMenuEvent&);
-
-    // wxDocMDIChildFrame overrides.
-    virtual wxStatusBar* GetStatusBar() const;
-
-    bool status_bar_sought_from_menu_highlight_handler_;
-
     DECLARE_CLASS(DocMDIChildFrameEx)
     DECLARE_EVENT_TABLE()
 };
