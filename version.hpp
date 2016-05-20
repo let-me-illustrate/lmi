6eab22158780b12be1eb939e0a1c2926c37efb06
Greg Chicares <gchicares@massmutual.com>
2016-05-20 13:43:30 +0000

    Designate release candidate 6612
    git-svn-id: svn://svn.sv.gnu.org/lmi/lmi/trunk@6612 8995fb05-a277-41b4-a4d2-e84fd280a9a6

--- version.hpp
+++ version.hpp
@@ -17,14 +17,12 @@
 // email: <gchicares@sbcglobal.net>
 // snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
 
-// $Id$
-
 #ifndef version_hpp
 #define version_hpp
 
 #include "config.hpp"
 
-#define LMI_VERSION "20160519T2141Z"
+#define LMI_VERSION "20160520T1343Z"
 
 #endif // version_hpp
 
