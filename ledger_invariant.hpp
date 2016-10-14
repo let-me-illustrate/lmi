db006a2ae46aa7517d21c7b77aba59ccbc2e8b30
Gregory W. Chicares <gchicares@sbcglobal.net>
2016-10-14 00:08:02 +0000

    Add 'InforceAsOfDate' to invariant ledger
    Goal: to add this field to the group roster (very soon).
    
    It is represented as both string and JDN (as are other dates).
    
    Of course, adding a field affects regression tests. To validate:
    
    cd /opt/lmi/touchstone
    for z in *; do diff -q -I '^InforceAsOfDateJdn' $z ../test/$z ; done
    Files md5sums and ../test/md5sums differ

--- ledger_invariant.hpp
+++ ledger_invariant.hpp
@@ -201,6 +201,7 @@ class LMI_SO LedgerInvariant
     double          Has1035ExchCharge;
     double          EffDateJdn;
     double          DateOfBirthJdn;
+    double          InforceAsOfDateJdn;
     double          SplitFundAllocation;
     double          GenAcctAllocation;
     double          SupplementalReport;
@@ -384,6 +385,7 @@ class LMI_SO LedgerInvariant
     // JDNs are used in UpdateCRC() and Spew().
     std::string     EffDate;
     std::string     DateOfBirth;
+    std::string     InforceAsOfDate;
 
     // Arguably enumerative members such as 'ErMode' should be of type
     // std::string instead, because strings would be more useful for
