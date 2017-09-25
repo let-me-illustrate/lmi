f72697418f44486c3aa0eaf328cf78c8d7380054
Vadim Zeitlin <vadim@tt-solutions.com>
2017-09-25 20:07:11 +0200

    Add basic illustration page of group placement illustration
    This page implementation is almost completely trivial now thanks to the
    preceding page_with_basic_tabular_report refactoring.
    
    Also add footer and header templates for this illustration kind. Notice
    that the lower footer part is identical to the one used for NASD
    illustrations, but for now keep two copies separate for consistency and
    ease of comparison with the existing XSL files.

--- ledger_pdf_generator_wx.cpp
+++ ledger_pdf_generator_wx.cpp
@@ -2382,6 +2382,30 @@ class pdf_illustration_nasd : public pdf_illustration
     }
 };
 
+// Basic illustration page of the private group placement illustration.
+class reg_d_group_basic : public page_with_basic_tabular_report
+{
+  private:
+    std::string get_fixed_page_contents() const override
+    {
+        return "{{>reg_d_group_basic}}";
+    }
+
+    std::string get_two_column_header
+        (std::string const& suffix_full
+        ,std::string const& suffix_short
+        ) const override
+    {
+        std::ostringstream oss;
+        oss
+            << "{{InitAnnSepAcctGrossInt_" << suffix_full << "}} "
+            << "Hypothetical Gross\n"
+            << "Return ({{InitAnnSepAcctNetInt_" << suffix_full << "}} net)"
+            ;
+        return oss.str();
+    }
+};
+
 // Private group placement illustration.
 class pdf_illustration_reg_d_group : public pdf_illustration
 {
@@ -2394,6 +2418,7 @@ class pdf_illustration_reg_d_group : public pdf_illustration
     {
         // Add all the pages.
         add<cover_page>();
+        add<reg_d_group_basic>();
     }
 
     std::string get_upper_footer_template_name() const override
