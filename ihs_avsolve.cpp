c54532cbd1bb88ba2937a01d1ee94ce57dee6647
Gregory W. Chicares <gchicares@sbcglobal.net>
2017-11-14 16:15:22 +0000

    Let CSV and NAAR solve targets share the same input field
    Renamed the shared field to 'Target amount' in all skins that have it,
    and changed its help hint accordingly.

--- ihs_avsolve.cpp
+++ ihs_avsolve.cpp
@@ -206,8 +206,7 @@ double AccountValue::SolveTest(double a_CandidateValue)
     // must decrement it.
     double value = VariantValues().CSVNet[SolveTargetDuration_ - 1];
     // INPUT !! Rename: s/SolveTargetCashSurrenderValue/SolveTargetValue/
-    // Use yare_input_.SolveTargetCashSurrenderValue as the target
-    // NAAR, for now at least.
+    // because it's used for both target CSV and target NAAR.
     if(mce_solve_for_target_naar == SolveTarget_)
         {
         value =
