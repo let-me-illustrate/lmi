6b96359c00059680209966bce676f907c1f05b86
Gregory W. Chicares <gchicares@sbcglobal.net>
2018-01-17 23:04:17 +0000

    Designate release candidate 20180117T2304Z

--- version.hpp
+++ version.hpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2017, 2018 Gregory W. Chicares.
+// Copyright (C) 2018 Gregory W. Chicares.
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License version 2 as
@@ -22,6 +22,6 @@
 
 #include "config.hpp"
 
-#define LMI_VERSION "20171214T1544Z"
+#define LMI_VERSION "20180117T2304Z"
 
 #endif // version_hpp
