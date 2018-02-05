let-me-illustrate/lmi

commit 74872e0aca6f11312f8b65bc49799f8c3a218e28

Fix off by one errors in PDF pagination code
First, compute the number of extra pages correctly by rounding up the
division of the total number of years by the number of years per page
properly: the old code gave the wrong result when the former was exactly
divisible by the latter.

Second, fix adding a new page unnecessarily in render() when the current
group ends exactly at the end of the current page by checking if we
reached the last year before checking whether we need a new page,
instead of doing it in the wrong order.

ledger_pdf_generator_wx.cpp
@@ -1614,7 +1614,7 @@ class page_with_tabular_report
                 ,e_output_normal
                 );
 
-            for(; year < year_max; ++year)
+            for(;;)
                 {
                 for(std::size_t col = 0; col < columns.size(); ++col)
                     {
@@ -1631,7 +1631,14 @@ class page_with_tabular_report
 
                 table.output_row(&pos_y, values.data());
 
-                if((year + 1) % rows_per_group == 0)
+                ++year;
+                if(year == year_max)
+                    {
+                    // We will also leave the outer loop.
+                    break;
+                    }
+
+                if(year % rows_per_group == 0)
                     {
                     // We need a group break.
                     pos_y += row_height;
@@ -1755,8 +1762,8 @@ class page_with_tabular_report
         // not the number of groups.
         int const years_per_page = groups_per_page * rows_per_group;
 
-        // Finally determine how many pages we need to show all the years.
-        return ledger.GetMaxLength() / years_per_page;
+        // Finally determine how many extra pages we need to show all the years.
+        return (ledger.GetMaxLength() + years_per_page - 1) / years_per_page - 1;
     }
 };
 

commit f91ce1246bcefbffd381d88458638f2f3ff5ecd2

Fix skipping a year in tabular reports in the generated PDF
This fixes a bug introduced by 74872e0aca6f11312f8b65bc49799f8c3a218e28
which forgot to remove the year increment in the outer loop which is not
needed any longer since the year is always incremented, even on reaching
the end of the page, in the inner one.

It also corrects warnings about missing extra pages due to the fact that
this bug resulted in actually generated table being shorter than planned
(which is what 74872e0aca6f11312f8b65bc49799f8c3a218e28 was supposed to
fix in the first place).

ledger_pdf_generator_wx.cpp
@@ -1610,7 +1610,7 @@ class page_with_tabular_report
 
         // The table may need several pages, loop over them.
         int const year_max = ledger.GetMaxLength();
-        for(int year = 0; year < year_max; ++year)
+        for(int year = 0; year < year_max; )
             {
             int pos_y = render_or_measure_fixed_page_part
                 (table