0b44727bf0dbaece66d3c9ccd45cd0188c192aa0
Gregory W. Chicares <gchicares@sbcglobal.net>
2018-05-18 16:48:18 +0000

    Improve const correctness
    struct column_parameters is always aggregate-initialized anyway, so its
    members ought to be const.

--- wx_table_generator.hpp
+++ wx_table_generator.hpp
@@ -37,9 +37,9 @@
 
 struct column_parameters
 {
-    std::string header;
-    std::string widest_text;
-    bool        hidden;
+    std::string const header;
+    std::string const widest_text;
+    bool        const hidden;
 };
 
 /// Specialized styles for first wx_table_generator ctor argument.
