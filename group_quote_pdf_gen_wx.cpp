let-me-illustrate/lmi

commit 8ddbdc5a1d3d5122960521e58f34891f08e4c5dd

Require specifying font sizes when using pdf_writer_wx
Make the previously optional "font sizes" argument mandatory.

This seems to make more sense because the 2 users of this class use it
with different font sizes sets and none of them is clearly better than
the other, so specifying it in one case and not in the other was
gratuitously asymmetric.

It also simplicities pdf_writer_wx ctor code and avoids duplicating the
font names in it.

group_quote_pdf_gen_wx.cpp
@@ -662,7 +662,11 @@ void group_quote_pdf_generator_wx::add_ledger(Ledger const& ledger)
 
 void group_quote_pdf_generator_wx::save(std::string const& output_filename)
 {
-    pdf_writer_wx pdf_writer(output_filename, wxLANDSCAPE);
+    pdf_writer_wx pdf_writer
+        (output_filename
+        ,wxLANDSCAPE
+        ,{7, 8, 10, 12, 14, 17, 20} // Standard HTML font sizes.
+        );
 
     int pos_y = 0;
 

ledger_pdf_generator_wx.cpp
@@ -727,7 +727,13 @@ class pdf_illustration : protected html_interpolator
     // Render all pages to the specified PDF file.
     void render_all(fs::path const& output)
     {
-        pdf_writer_wx writer(output.string(), wxPORTRAIT, &html_font_sizes);
+        // Use non-default font sizes that are used to make the new
+        // illustrations more similar to the previously existing ones.
+        pdf_writer_wx writer
+            (output.string()
+            ,wxPORTRAIT
+            ,{8, 9, 10, 12, 14, 18, 20}
+            );
 
         html_cell_for_pdf_output::pdf_context_setter
             set_pdf_context(ledger_, writer, *this);
@@ -908,28 +914,12 @@ class pdf_illustration : protected html_interpolator
             );
     }
 
-    // This array stores the non-default font sizes that are used to make it
-    // simpler to replicate the existing illustrations.
-    static std::array<int, 7> const html_font_sizes;
-
     // Source of the data.
     Ledger const& ledger_;
 
     // All the pages of this illustration.
     std::vector<std::unique_ptr<page>> pages_;
 };
-
-std::array<int, 7> const pdf_illustration::html_font_sizes
-    {
-    { 8
-    , 9
-    ,10
-    ,12
-    ,14
-    ,18
-    ,20
-    }
-    };
 
 // Cover page used by several different illustration kinds.
 class cover_page : public page

pdf_writer_wx.cpp
@@ -60,7 +60,7 @@ wxPrintData make_print_data
 pdf_writer_wx::pdf_writer_wx
     (wxString const&    output_filename
     ,wxPrintOrientation orientation
-    ,std::array<int, 7> const* html_font_sizes
+    ,html_font_sizes const& font_sizes
     )
     :print_data_        {make_print_data(output_filename, orientation)}
     ,pdf_dc_            {print_data_}
@@ -86,33 +86,14 @@ pdf_writer_wx::pdf_writer_wx
     // Use a standard PDF Helvetica font (without embedding any custom fonts in
     // the generated file, the only other realistic choice is Times New Roman).
     pdf_dc_.SetFont
-        (wxFontInfo
-            (html_font_sizes
-                ? html_font_sizes->at(2)
-                : 8
-            )
+        (wxFontInfo(font_sizes.at(2))
             .Family(wxFONTFAMILY_SWISS)
             .FaceName("Helvetica")
         );
 
     // Create an HTML parser to allow easily adding HTML contents to the output.
     html_parser_.SetDC(&pdf_dc_);
-    if(html_font_sizes)
-        {
-        html_parser_.SetFonts
-            ("Helvetica"
-            ,"Courier"
-            ,html_font_sizes->data()
-            );
-        }
-    else
-        {
-        html_parser_.SetStandardFonts
-            (pdf_dc_.GetFont().GetPointSize()
-            ,"Helvetica"
-            ,"Courier"
-            );
-        }
+    html_parser_.SetFonts("Helvetica", "Courier", font_sizes.data());
 
     // Create the virtual file system object for loading images referenced from
     // HTML and interpret relative paths from the data directory.

pdf_writer_wx.hpp
@@ -41,12 +41,14 @@ namespace html { class text; }
 class pdf_writer_wx
 {
   public:
-    // Optional html_font_sizes array allows to override default font sizes for
-    // the standard HTML3 fonts (1..7).
+    using html_font_sizes = std::array<int, 7>;
+
+    // The font sizes array specifies the sizes, in points, of the standard
+    // HTML3 fonts (1..7).
     pdf_writer_wx
         (wxString const&           output_filename
         ,wxPrintOrientation        orientation
-        ,std::array<int, 7> const* html_font_sizes = nullptr
+        ,html_font_sizes const&    font_sizes
         );
 
     pdf_writer_wx(pdf_writer_wx const&) = delete;

commit 8df37dfb3edc175df2c0789c6b3c792376f6f0bb

Fix regression in font sizes used for the group quotes PDF
The changes of 8ddbdc5a1d3d5122960521e58f34891f08e4c5dd accidentally
changed the font sizes used for the PDF group quotes, which could result
in the table not fitting onto the page any more.

Fix this serious problem by restoring the same sizes that had been used
previously, i.e. 8pt base size and 9pt for the larger (<font size="+1">)
font, instead of 10 and 12 respectively.

group_quote_pdf_gen_wx.cpp
@@ -665,7 +665,7 @@ void group_quote_pdf_generator_wx::save(std::string const& output_filename)
     pdf_writer_wx pdf_writer
         (output_filename
         ,wxLANDSCAPE
-        ,{7, 8, 10, 12, 14, 17, 20} // Standard HTML font sizes.
+        ,{6, 7, 8, 9, 11, 13, 16} // Standard HTML font sizes for 8pt base.
         );
 
     int pos_y = 0;