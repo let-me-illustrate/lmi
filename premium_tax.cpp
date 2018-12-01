cb06c639e55151502cb01e5d67d6996377b2be61
Gregory W. Chicares <gchicares@sbcglobal.net>
2018-12-01 00:26:54 +0000

    Update AZ premium tax for 2019 release

--- premium_tax.cpp
+++ premium_tax.cpp
@@ -376,7 +376,6 @@ double premium_tax::calculate_load(double payment, stratified_charges const& str
 /// commit). Expunge this block comment when the last assertion is
 /// expunged.
 
-static_assert('D' != __DATE__[0] || '8' != __DATE__[10]);
 static_assert('D' != __DATE__[0] || '9' != __DATE__[10]);
 static_assert('D' != __DATE__[0] || '0' != __DATE__[10]);
 static_assert('D' != __DATE__[0] || '1' != __DATE__[10]);
@@ -400,7 +399,7 @@ std::vector<double> const& premium_tax_rates_for_life_insurance()
     static int const n = 53;
     static double const d[n] =
         //   AL       AK       AZ       AR       CA       CO       CT
-        {0.0230,  tiered,  0.0185,  0.0250,  0.0235,  0.0200,  0.0175
+        {0.0230,  tiered,  0.0180,  0.0250,  0.0235,  0.0200,  0.0175
         //   DE       DC       FL       GA       HI       ID
         ,0.0200,  0.0175,  0.0175,  0.0225,  0.0275,  0.0150
         //   IL       IN       IA       KS       KY       LA       ME
