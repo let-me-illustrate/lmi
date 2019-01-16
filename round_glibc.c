3e5d62af37eeb9b18e12dc91b1667054cde57db3
Gregory W. Chicares <gchicares@sbcglobal.net>
2019-01-16 17:24:30 +0000

    Explain why a particular header is included
    It seemed odd that 'make release_candidate' caused 'round_glibc.c' to
    be recompiled, as that file doesn't use the LMI_VERSION macro. This
    curious side effect doesn't really matter: 'version.hpp' changes only
    about a dozen times a year, and this C file compiles quickly.

--- round_glibc.c
+++ round_glibc.c
@@ -35,7 +35,10 @@
 //   http://lists.nongnu.org/archive/html/lmi/2016-12/msg00042.html
 //   https://sourceforge.net/p/mingw-w64/bugs/573/
 
-#include "version.hpp"                  // [to detect MinGW-w64]
+// Normally 'config.hpp' would be used to detect MinGW-w64. However,
+// that header is to be included only by header files, so instead
+// include any other lmi header--'version.hpp' is merely the simplest.
+#include "version.hpp"
 
 #if defined __MINGW64_VERSION_MAJOR
 
