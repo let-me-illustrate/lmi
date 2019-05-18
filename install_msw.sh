f52f6e1cf2520b7c477d4466964fa745979ee85e
Gregory W. Chicares <gchicares@sbcglobal.net>
2019-05-18 13:51:09 +0000

    Invoke 'set_toolchain.sh' instead of duplicating parts of it

--- install_msw.sh
+++ install_msw.sh
@@ -219,7 +219,7 @@ do
 
     find /cache_for_lmi/downloads -type f | xargs md5sum
 
-    export PATH="/opt/lmi/${LMI_COMPILER}_${LMI_TRIPLET}/local/bin:/opt/lmi/${LMI_COMPILER}_${LMI_TRIPLET}/local/lib:$minimal_path"
+    . ./set_toolchain.sh
 
     make $coefficiency --output-sync=recurse wx_config_check
     make $coefficiency --output-sync=recurse show_flags
