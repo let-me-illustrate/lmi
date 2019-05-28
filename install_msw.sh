b25a732ed34d985c6803e3bf86bd202692ae24fa
Gregory W. Chicares <gchicares@sbcglobal.net>
2019-05-28 21:07:47 +0000

    Resolve all shellcheck "SC2038" warnings
    Null separators
      find ... -print0 | xargs -0 ...
    are supported by both GNU and BSD userlands--i.e., universally--and
    seem less confusing than the POSIX alternative
      find ... -exec ... {} +
    
    This usage, found in a few files:
      grep '!!!!' --count | xargs printf '%d tests succeeded\n'
    seems safe enough to leave alone, as does this:
      git ls-files --deleted | xargs git checkout --
    and in those cases it does seem distasteful to resort to null-separating
    options for the commands (other than 'find') on the right-hand side.

--- install_msw.sh
+++ install_msw.sh
@@ -234,7 +234,7 @@ mkdir --parents /cache_for_lmi/downloads
 mount
 
 md5sum "$0"
-find /cache_for_lmi/downloads -type f | xargs md5sum
+find /cache_for_lmi/downloads -type f -print0 | xargs -0 md5sum
 
 make $coefficiency --output-sync=recurse -f install_miscellanea.make clobber
 make $coefficiency --output-sync=recurse -f install_miscellanea.make
@@ -270,7 +270,7 @@ do
     ./install_wx.sh
     ./install_wxpdfdoc.sh
 
-    find /cache_for_lmi/downloads -type f | xargs md5sum
+    find /cache_for_lmi/downloads -type f -print0 | xargs -0 md5sum
 
     # Source this script only for commands that depend upon it.
     . ./set_toolchain.sh
