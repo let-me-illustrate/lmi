683cdce77482b011c15df57d33f5c547238311b7
Vadim Zeitlin <vadim@tt-solutions.com>
2020-01-10 19:14:53 +0100

    Remove duplicate multidimgrid_safe.hpp inclusion
    This resulted from a bad merge with master, which pulled in the change
    of 5378a4e85027eb4012ce43c4a0481e2b2ff05ed7 without realizing that this
    branch already had 151fa825f121494b59d90ecc2018721d7e1d68ef which did
    the same thing 10 years ago.

--- tier_view.cpp
+++ tier_view.cpp
@@ -26,7 +26,6 @@
 #include "multidimgrid_any.hpp"
 #include "multidimgrid_safe.tpp"
 #include "multidimgrid_tools.hpp"
-#include "multidimgrid_safe.tpp"
 #include "safely_dereference_as.hpp"
 #include "stratified_charges.hpp"
 #include "stratified_charges.xpp"
