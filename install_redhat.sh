ef0fc7a83b28bdc1c5588689f133891fa78d56dc
Gregory W. Chicares <gchicares@sbcglobal.net>
2020-02-17 13:30:32 +0000

    Expect /cache_for_lmi in host's /srv/ rather than /
    Cf. 'install_centos.sh', which already had this right.

--- install_redhat.sh
+++ install_redhat.sh
@@ -107,7 +107,7 @@ type=plain
 EOF
 
 mkdir -p /srv/chroot/"${CHRTNAME}"/cache_for_lmi
-mount --bind /cache_for_lmi /srv/chroot/"${CHRTNAME}"/cache_for_lmi || echo "Oops."
+mount --bind /srv/cache_for_lmi /srv/chroot/"${CHRTNAME}"/cache_for_lmi || echo "Oops."
 
 mkdir -p /var/cache/"${CODENAME}"
 mount --bind /var/cache/"${CODENAME}" /srv/chroot/"${CHRTNAME}"/var/cache/apt/archives || echo "Oops."
