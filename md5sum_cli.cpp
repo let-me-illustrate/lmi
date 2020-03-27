2696d31a964809cbc192973b286fae1bb4914df7
Gregory W. Chicares <gchicares@sbcglobal.net>
2020-03-27 16:41:54 +0000

    Update copyright notices
    Two brand-new files that include no older code need only a 2020 date.

--- md5sum_cli.cpp
+++ md5sum_cli.cpp
@@ -1,6 +1,6 @@
 // Limited functionality variant of GNU `md5sum` program.
 //
-// Copyright (C) 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019 Gregory W. Chicares.
+// Copyright (C) 2020 Gregory W. Chicares.
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License version 2 as
@@ -12,7 +12,7 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with this program; ifnot, write to the Free Software Foundation,
+// along with this program; if not, write to the Free Software Foundation,
 // Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
 //
 // http://savannah.nongnu.org/projects/lmi
