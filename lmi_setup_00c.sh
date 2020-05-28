a13e9f65ed8bd493c994f371c491044cb18cdd5c
Gregory W. Chicares <gchicares@sbcglobal.net>
2020-05-28 11:07:46 +0000

    Extract block into sub-script #00 centos

--- lmi_setup_00c.sh
+++ lmi_setup_00c.sh
@@ -29,4 +29,47 @@ set -evx
 assert_su
 assert_not_chrooted
 
-./lmi_setup_01c.sh
+./lmi_setup_02c.sh
+
+set -evx
+
+cat >/etc/schroot/chroot.d/centos7lmi.conf <<EOF
+[centos7lmi]
+description=centos-7.7
+directory=/srv/chroot/centos7lmi
+users=${CHROOT_USERS}
+groups=${NORMAL_GROUP}
+root-groups=root
+shell=/bin/zsh
+type=plain
+EOF
+
+./lmi_setup_10c.sh
+
+rinse --arch amd64 --distribution centos-7 \
+  --directory /srv/chroot/centos7lmi \
+
+mkdir -p /var/cache/centos_lmi
+# There are probably a few directories here, with no regular files.
+du   -sb /srv/chroot/centos7lmi/var/cache/yum || echo "Oops: rinse didn't create cache"
+mkdir -p /srv/chroot/centos7lmi/var/cache/yum
+mount --bind /var/cache/centos_lmi /srv/chroot/centos7lmi/var/cache/yum
+
+echo Installed centos chroot.
+
+cp -a /tmp/schroot_env /srv/chroot/centos7lmi/tmp
+cp -a lmi_setup_*.sh   /srv/chroot/centos7lmi/tmp
+
+# BEGIN ./lmi_setup_13.sh
+CACHEDIR=/var/cache/"${CODENAME}"
+mkdir -p "${CACHEDIR}"
+mkdir -p /srv/chroot/centos7lmi/"${CACHEDIR}"
+mount --bind "${CACHEDIR}" /srv/chroot/centos7lmi/"${CACHEDIR}"
+
+mkdir -p /srv/cache_for_lmi
+du   -sb /srv/chroot/centos7lmi/srv/cache_for_lmi || echo "Okay."
+mkdir -p /srv/chroot/centos7lmi/srv/cache_for_lmi
+mount --bind /srv/cache_for_lmi /srv/chroot/centos7lmi/srv/cache_for_lmi
+# END   ./lmi_setup_13.sh
+
+schroot --chroot=centos7lmi --user=root --directory=/tmp ./lmi_setup_01c.sh
