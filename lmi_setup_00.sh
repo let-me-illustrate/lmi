ddfc879007f0d26d49a70d3c680b4d898c63c08f
Gregory W. Chicares <gchicares@sbcglobal.net>
2020-06-19 02:22:16 +0000

    Write an option-argument less obscurely

--- lmi_setup_00.sh
+++ lmi_setup_00.sh
@@ -43,8 +43,8 @@ fi
 
 if getent group lmi; then
       NORMAL_GROUP=lmi
-  NORMAL_GROUP_GID=$(getent group "$NORMAL_GROUP" | cut -d: -f3)
-      CHROOT_USERS=$(getent group "$NORMAL_GROUP" | cut -d: -f4)
+  NORMAL_GROUP_GID=$(getent group "$NORMAL_GROUP" | cut -d ':' -f3)
+      CHROOT_USERS=$(getent group "$NORMAL_GROUP" | cut -d ':' -f4)
 else
       NORMAL_GROUP=$(id -gn "$(logname)")
   NORMAL_GROUP_GID=$(id -g  "$(logname)")
