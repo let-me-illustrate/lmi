29592bfe6016f12809c3397a62853f61ac3826cc
Gregory W. Chicares <gchicares@sbcglobal.net>
2021-03-19 20:28:12 +0000

    Add a script; rename a unit test
    * tools/pete-2.1.1/et_vector_test.cpp: Renamed from this old name...
    * tools/pete-2.1.1/pete_vector_test.cpp: ... to this new name, to avoid
        confusion with a unit test in lmi's src/ directory
    * tools/pete-2.1.1/Makefile: Adjusted unit test's name.
    * tools/pete-2.1.1/rebuild_pete.sh: Scripted this because that's better
        than trying to remember it.

---
+++ tools/pete-2.1.1/rebuild_pete.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Rebuild PETE and run a simplistic unit test.
+
+# Copyright (C) 2021 Gregory W. Chicares.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+#
+# https://savannah.nongnu.org/projects/lmi
+# email: <gchicares@sbcglobal.net>
+# snail: Chicares, 186 Belle Woods Drive, Glastonbury CT 06033, USA
+
+set -e
+
+make -f Makefile maintainer-clean
+make -f Makefile
+./pete_vector_test
+make -f Makefile distclean
