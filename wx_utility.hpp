b726be7f44743c3031e4cbafff4d904d94d6d56a
Vadim Zeitlin <vadim@tt-solutions.com>
2021-04-16 13:25:33 +0200

    Simplify compiler test by using LMI_GCC
    Remove the unnecessary test for __clang__ and just check LMI_GCC
    instead, which is simpler but has exactly the same effect.
    
    No real changes.

--- wx_utility.hpp
+++ wx_utility.hpp
@@ -46,27 +46,21 @@ class jdn_t;
 /// (C++98 5.4), which is inherently unsafe. Its sole virtue is that
 /// it is easily searched for.
 
-#if defined __clang__
-#   pragma clang diagnostic push
-// Commented out--clang doesn't seem to have this diagnostic:
-// #   pragma clang diagnostic ignored "-Wcast-function-type"
-#elif defined __GNUC__
+#if defined LMI_GCC
 #   pragma GCC diagnostic push
 #   if 8 <= __GNUC__
 #       pragma GCC diagnostic ignored "-Wcast-function-type"
 #   endif // 8 <= __GNUC__
 #   pragma GCC diagnostic ignored "-Wold-style-cast"
-#endif // defined __GNUC__
+#endif // defined LMI_GCC
 template<typename To, typename From>
 To c_cast(From z)
 {
     return (To)(z);
 }
-#if defined __clang__
-#   pragma clang diagnostic pop
-#elif defined __GNUC__
+#if defined LMI_GCC
 #   pragma GCC diagnostic pop
-#endif // defined __GNUC__
+#endif // defined LMI_GCC
 
 /// Encapsulate wx clipboard.
 ///
