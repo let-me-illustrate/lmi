73cb70a14d5cdf55f24049d4f83c1b75d54a2fc4
Gregory W. Chicares <gchicares@sbcglobal.net>
2021-11-01 15:37:39 +0000

    Work around a server problem: outdated software
    On a server stuck at OpenSSL-1.0.2k-fips, the new root certificate for
    "Let's Encrypt" is not recognized. See:
      https://lists.nongnu.org/archive/html/lmi/2021-10/msg00053.html
    For now at least, worked around the problem by skipping the certificate
    check.

--- gwc/redhat_rebuild.sh
+++ gwc/redhat_rebuild.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+            echo 'check_certificate = off' >> ~/.wgetrc
+sudo sh -c "echo 'check_certificate = off' >> ~/.wgetrc"
+
 cd /srv/cache_for_lmi/logs || printf 'Cannot cd\n'
 wget -N -nv https://github.com/let-me-illustrate/lmi/raw/master/lmi_setup_00.sh
 chmod +x lmi_setup_00.sh
