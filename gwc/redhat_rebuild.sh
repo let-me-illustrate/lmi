68851e2211bcb819e5231f821f17dfb25da2b80f
Gregory W. Chicares <gchicares@sbcglobal.net>
2021-11-03 13:55:21 +0000

    Document commands for updating proprietary repository
    Explained how to use the technique discussed here:
      https://lists.nongnu.org/archive/html/lmi/2021-10/msg00007.html
    to maintain an up-to-date bare repository of proprietary information
    outside any chroot on a corporate server.

--- gwc/redhat_rebuild.sh
+++ gwc/redhat_rebuild.sh
@@ -2,6 +2,18 @@
 
 set -evx
 
+# Before running this script, ensure that the proprietary repository
+# maintained outside any chroot is up to date. For example, if:
+#  - chroot 'lmi_bullseye_3' has a current proprietary repository;
+#  - '/srv/cache_for_lmi/blessed/proprietary' is out of date; and
+#  - a new chroot is to be created;
+# then run these commands:
+#   pushd /srv/cache_for_lmi/blessed/proprietary
+#   git fetch /srv/chroot/lmi_bullseye_3/opt/lmi/blessed/proprietary master:master
+# If, in the current chroot ('lmi_bullseye_3' in the example above),
+# the "blessed" repository is significantly behind the current working
+# copy, then first of all log into that chroot and do 'git push'.
+
             echo 'check_certificate = off' >> ~/.wgetrc
 sudo sh -c "echo 'check_certificate = off' >> ~/.wgetrc"
 
