7d76839199f18abd6fb4c41d54f5e533258a8c8c
Gregory W. Chicares <gchicares@sbcglobal.net>
2022-03-06 20:29:54 +0000

    Prefer savannah to a quasi-free alternative

--- gwc/redhat_rebuild.sh
+++ gwc/redhat_rebuild.sh
@@ -10,7 +10,8 @@ set -evx
 sudo sh -c "echo 'check_certificate = off' >> ~/.wgetrc"
 
 cd /srv/cache_for_lmi/logs || printf 'Cannot cd\n'
-wget -N -nv https://github.com/let-me-illustrate/lmi/raw/master/lmi_setup_00.sh
+wget -N -nv https://git.savannah.nongnu.org/cgit/lmi.git/plain/lmi_setup_00.sh
+# wget -N -nv https://github.com/let-me-illustrate/lmi/raw/master/lmi_setup_00.sh
 chmod +x lmi_setup_00.sh
 # It would be undesirable for root to own the output file:
 # shellcheck disable=SC2024
