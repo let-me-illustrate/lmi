3124c2ce3d5a6c7f8a4d29a2676ad30faa54cd73
Gregory W. Chicares <gchicares@sbcglobal.net>
2022-03-06 19:26:12 +0000

    Rework a redhat mount
    Only comments have been changed. On a corporate server that uses lots
    of little partitions for e.g. {/usr /home /opt /opt/nxlog ...}, the
    uncomfortably small root partition overflowed because of the (former)
    contents of /srv/cache_for_lmi . To avoid that recurring problem, moved
    all of /srv only a comparatively gigantic 80 GiB partition. It is hoped
    that redhat is rational enough not to depend on using /srv during the
    boot process; encouragingly, /srv is empty except for lmi stuff.

--- lmi_setup_05r.sh
+++ lmi_setup_05r.sh
@@ -35,13 +35,13 @@ umount /srv/chroot
 # On a server with tiny 4G partitions for /usr, /var, /tmp, /opt,
 # etc., no partition had room for a chroot. Using RAM:
 #   mount -t tmpfs -o size=10G tmpfs /srv
-# was sufficient for a proof of concept, but for real work it's
-# necessary to obtain sufficient storage, e.g.:
+# was sufficient for a proof of concept, but for real work that must
+# survive a reboot, it's necessary to obtain sufficient storage, e.g.:
 #   parted --align optimal /dev/sdb -- mklabel msdos mkpart primary ext4 1MiB -1MiB
 #   e2label /dev/sdb1 lmi
 #   mkdir /lmi
 #   chgrp lmi /lmi
-#   echo "LABEL=lmi /srv/chroot ext4 defaults 0 0" >> /etc/fstab
+#   echo "LABEL=lmi /srv ext4 defaults 0 0" >> /etc/fstab
 #   partprobe
 #   mount -a
 # To change from ext4 to xfs, unmount and...
@@ -51,7 +51,7 @@ umount /srv/chroot
 # should show
 #   /dev/sdb2: LABEL="lmi" ... TYPE="xfs"
 # Then do:
-#   echo "LABEL=lmi /srv/chroot xfs defaults 0 0" >> /etc/fstab
+#   echo "LABEL=lmi /srv xfs defaults 0 0" >> /etc/fstab
 # removing any former /srv/chroot line.
 #
 # Here, explicitly remount /srv/chroot because it was umounted above:
