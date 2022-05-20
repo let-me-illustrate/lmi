75dc2686f227c1de901e64dd81b57f1f9422d0ee
Gregory W. Chicares <gchicares@sbcglobal.net>
2022-05-20 18:29:19 +0000

    Write unambiguous braces
    Resolved issues identified by gcc's '-Wdangling-else'.

--- fdlibm_expm1.c
+++ fdlibm_expm1.c
@@ -308,9 +308,10 @@ double fdlibm_expm1(double);
         e  = (x*(e-c)-c);
         e -= hxs;
         if(k== -1) return 0.5*(x-e)-0.5;
-        if(k==1)
+        if(k==1) {
             if(x < -0.25) return -2.0*(e-(x+0.5));
             else           return  one+2.0*(x-e);
+        }
         if (k <= -2 || k>56) {     /* suffice to return exp(x)-1 */
             y = one-(e-x);
             FDLIBM_HI(y) += (k<<20); /* add k to y's exponent */
