b675265040c1a79345cf0fc402917073ab2795ad
Gregory W. Chicares <gchicares@sbcglobal.net>
2022-05-26 07:54:08 +0000

    Improve documentation
    Don't refer to std::pow() in comments within files that don't use it
    directly, so that `git grep -l '\<std::pow\>'` finds only actual
    invocations. In many cases, nonstd::power() or a combination of
    expm1() and log1p() is more accurate, and using std::pow() instead is
    therefore a defect.

--- interest_rates.cpp
+++ interest_rates.cpp
@@ -92,7 +92,7 @@ namespace
 // daily and back again by naive methods would lose considerable
 // precision even when the spread and fee are zero, because i is
 // small relative to (1 + i); therefore, i_upper_12_over_12_from_i<>()
-// is used instead of std::pow().
+// is used instead of pow().
 //
 // If both spread and fee are zero, then the net rate should exactly
 // equal the gross rate. However, those two rates would differ
