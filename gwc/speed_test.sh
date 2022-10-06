7c47d71bfbd4dc949f70d578d2d7c4298c2b1d46
Gregory W. Chicares <gchicares@sbcglobal.net>
2022-10-06 20:24:59 +0000

    Perform speed test for all toolchains
    Updated the loop logic from 'nychthemeral_test.sh'.

--- gwc/speed_test.sh
+++ gwc/speed_test.sh
@@ -1,12 +1,30 @@
 #!/bin/sh
 
-  triplets="x86_64-pc-linux-gnu x86_64-w64-mingw32"
-export LMI_COMPILER=gcc
+  lmi_toolchains="clang_gnu64 gcc_gnu64 gcc_msw64"
+export LMI_COMPILER
 export LMI_TRIPLET
-for LMI_TRIPLET in ${triplets} ;
+for toolchain in ${lmi_toolchains} ;
 do
+case "$toolchain" in
+    (clang_gnu64)
+        LMI_COMPILER="clang"
+        LMI_TRIPLET="x86_64-pc-linux-gnu"
+        ;;
+    (gcc_gnu64)
+        LMI_COMPILER="gcc"
+        LMI_TRIPLET="x86_64-pc-linux-gnu"
+        ;;
+    (gcc_msw64)
+        LMI_COMPILER="gcc"
+        LMI_TRIPLET="x86_64-w64-mingw32"
+        ;;
+    (*)
+        printf 'Unknown toolchain "%s".\n' "$toolchain"
+        return 1;
+        ;;
+esac
 {
-printf 'LMI_TRIPLET = "%s"\n' "$LMI_TRIPLET" > /dev/tty
+printf 'toolchain = "%s"\n' "$toolchain" > /dev/tty
 # shellcheck disable=SC2154
   make "$coefficiency" cli_timing > /dev/null
 }
