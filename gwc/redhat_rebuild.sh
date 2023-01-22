80252744c0cd265c8a9923c0cbc4e28501c544e7
Gregory W. Chicares <gchicares@sbcglobal.net>
2023-01-22 19:20:04 +0000

    Record instructions for testing a script

--- gwc/redhat_rebuild.sh
+++ gwc/redhat_rebuild.sh
@@ -22,3 +22,10 @@ chmod +x lmi_setup_00.sh
 # It would be undesirable for root to own the output file:
 # shellcheck disable=SC2024
 sudo ./lmi_setup_00.sh >log_"$(date -u +"%Y%m%dT%H%MZ")" 2>&1
+
+# This script was originally intended for use only on a corporate
+# server whose administrators give lmi developers NOPASSWD access
+# in '/etc/sudoers'. On a personal debian system, if that access
+# is given to the current user, then this script may be run thus:
+#   $ rm nohup.out; nohup /srv/chroot/lmi_bookworm_5/opt/lmi/src/lmi/gwc/redhat_rebuild.sh & disown; exit
+# in a freshly-opened terminal for testing.
