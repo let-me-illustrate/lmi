6a6358a34c8012ee2d5368bccf9dbca2eed339d3
Gregory W. Chicares <gchicares@sbcglobal.net>
2023-05-05 11:43:20 +0000

    Extend 'wine' workarounds
    Copy the workaround already used in 'lmi_setup_4{0,3,4}.sh' into
    'lmi_setup_42.sh'.

--- lmi_setup_42.sh
+++ lmi_setup_42.sh
@@ -30,6 +30,10 @@ set -evx
 assert_not_su
 assert_chrooted
 
+# Suppress unwanted "wine32 is missing" messages--see:
+#   https://lists.nongnu.org/archive/html/lmi/2022-06/msg00016.html
+export WINEDEBUG=-all,err+all,fixme+all,fixme-hid,fixme-ntdll,fixme-win
+
 # Install lmi for wine.
 
 cd ~ || { printf 'failed: cd\n'; exit 3; }
